1a8cb4f87bfb69abe7f8a994ec3fd18af2aee67f
tpielok <31277975+tpielok@users.noreply.github.com>
2017-08-30 10:23:53 +0000

    working on onmvcov

--- test/onlinestats.jl
+++ test/onlinestats.jl
@@ -2,8 +2,13 @@
 
 using BAT
 using Base.Test
+using StatsBase
 
 @testset "onlinestats" begin
+    n = 6
+    data1  = [(-1)^x*exp(x-n/2) for x in 1:n]
+    data2 = flipdim(data1, 1) 
+
     @testset "BAT.kbn_add" begin
         bg = +1E5
         sm = +1e-4
@@ -25,7 +30,6 @@ using Base.Test
         @test typeof(@inferred BAT.kbn_add((0.0f0, 0.0f0), (0.0f0, 0.0f0))) == NTuple{2, Float32}
     end
     @testset "BAT.OnlineMvMean" begin
-        n = 3
         @test typeof(@inferred BAT.OnlineMvMean(n)) <: AbstractVector{Float64}
         @test typeof(@inferred BAT.OnlineMvMean{Float32}(n)) <: AbstractVector{Float32}
 
@@ -33,5 +37,28 @@ using Base.Test
         mvmean2 = BAT.OnlineMvMean(n)
 
         @test size(mvmean1)[1] == n
+        
+        w1 = 0.5
+        w2 = 1.2
+
+        push!(mvmean1, data1, w1)
+        @test mvmean1 ≈ data1
+        push!(mvmean1, data2, w2)
+        @test mvmean1 ≈ (w1*data1 + w2*data2)/(w1 + w2)
+
+        push!(mvmean2, data2, w2)
+        merge!(mvmean1, mvmean2)
+        @test mvmean1 ≈ (w1*data1 + w2*data2 + w2*data2)/(w1 + w2 + w2)
+    end
+    @testset "OnlineMvCov" begin
+        @test typeof(@inferred BAT.OnlineMvCov(n)) <: AbstractMatrix{Float64}
+        @test typeof(@inferred BAT.OnlineMvCov{Float32, ProbabilityWeights}(n)) <: AbstractMatrix{Float32}
+        data = hcat(1.:3,1.:2:2*3)
+        mvcov = BAT.OnlineMvCov{Float64, ProbabilityWeights}(2)
+        for i in indices(data, 1)
+            push!(mvcov, data[i,:]);
+        end
+
+        @test mvcov ≈ cov(data, ProbabilityWeights(ones(3)), 1; corrected = true)
     end
 end
