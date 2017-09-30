7beaba8e0277e184e046f012c8b3110ff5168980
Oliver Schulz <oschulz@mpp.mpg.de>
2017-09-30 14:16:50 +0200

    MCMCDataVector

--- src/mcmc_samplevector.jl
+++ src/mcmc_samplevector.jl
@@ -1,10 +1,25 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
 
-struct MCMCSampleVector{P<:Real,T<:AbstractFloat,W<:Real} <: DenseVector{MCMCSample{P,T,W}}
+abstract type MCMCDataVector{T} <: DenseVector{T} end
+
+mcmc_callback(X::MCMCDataVector, args...) = MCMCPushCallback(X, args...)
+
+function Base.merge!(X::MCMCDataVector, Xs::MCMCDataVector...)
+    for Y in Xs
+        append!(X, Y)
+    end
+    X
+end
+
+Base.merge(X::MCMCDataVector, Xs::MCMCDataVector...) = merge!(deepcopy(X), Xs...)
+
+
+
+struct MCMCSampleVector{P<:Real,T<:AbstractFloat,W<:Real} <: MCMCDataVector{MCMCSample{P,T,W}}
     params::ExtendableArray{P, 2, 1}
-    log_values::Vector{T}
-    weights::Vector{W}
+    log_value::Vector{T}
+    weight::Vector{W}
 end
 
 export MCMCSampleVector
@@ -19,16 +34,16 @@ function MCMCSampleVector(chain::MCMCChain)
 end
 
 
-Base.size(xs::MCMCSampleVector) = size(xs.log_values)
+Base.size(xs::MCMCSampleVector) = size(xs.log_value)
 
 Base.getindex(xs::MCMCSampleVector{P,T,W}, i::Integer) where {P,T,W} =
-    MCMCSample{P,T,W}(xs.params[:,i], xs.log_values[i], xs.weights[i])
+    MCMCSample{P,T,W}(xs.params[:,i], xs.log_value[i], xs.weight[i])
 
 
 function Base.push!(xs::MCMCSampleVector, x::MCMCSample)
     append!(xs.params, x.params)
-    push!(xs.log_values, x.log_value)
-    push!(xs.weights, x.weight)
+    push!(xs.log_value, x.log_value)
+    push!(xs.weight, x.weight)
     xs
 end
 
@@ -46,102 +61,45 @@ function Base.push!(xs::MCMCSampleVector, chain::MCMCChain)
     chain
 end
 
-
-mcmc_callback(sv::MCMCSampleVector, args...) = MCMCPushCallback(sv, args...)
-
-
-# ToDo: merge/append for MCMCSampleVector
-
-
-#=
-
-
-struct MCMCChainInfoVector <: DenseVector{MCMCChainInfo}
-    id::Vector{Int32}
-    cycle::Vector{Int32}
-    tuned::Vector{Bool}
-    converged::Vector{Bool}
-end
-
-export MCMCChainInfoVector
-
-MCMCChainInfoVector() =
-    MCMCChainInfoVector((Vector{Int32}(), Vector{Int32}(), Vector{Bool}(), Vector{Bool}()))
-
-
-Base.size(xs::MCMCChainInfoVector) = size(xs.id)
-
-Base.getindex(xs::MCMCChainInfoVector, i::Integer)  =
-    MCMCChainInfo(xs.id[i], xs.cycle[i], xs.tuned[i], xs.converged[i])
-
-
-function Base.push!(xs::MCMCChainInfoVector, x::MCMCChainInfo)
-    push!(xs.id, x.id)
-    push!(xs.cycle, x.cycle)
-    push!(xs.tuned, x.tuned)
-    push!(xs.converged, x.converged)
-    xs
-end
-
-function Base.push!(xs::MCMCChainInfoVector, chain::MCMCChain)
-    push!(xs, chain XXX nfo)
-    chain
+function Base.append!(A::MCMCSampleVector, B::MCMCSampleVector)
+    append!(A.params, B.params)
+    append!(A.log_value, B.log_value)
+    append!(A.weight, B.weight)
+    A
 end
 
 
-mcmc_callback(sv::MCMCChainInfoVector, args...) = MCMCPushCallback(sv, args...)
 
-
-
-mutable struct TaggedMCMCSample{
-    S<:AbstractMCMCSample
-} <: AbstractMCMCSample
-    sample::S
-    sampleno::Int64
-    chaininfo::MCMCChainInfo
-end
-
-export MCMCSample
-
-
-struct TaggedMCMCSampleVector{S<:AbstractMCMCSample} <: DenseVector{TaggedMCMCSample{S}}
-    sample::S
+struct MCMCSampleIDVector <: MCMCDataVector{MCMCSampleID}
+    chainid::Vector{Int32}
+    chaincycle::Vector{Int32}
     sampleno::Vector{Int64}
-    chaininfo::MCMCChainInfoVector
 end
 
-export TaggedMCMCSampleVector
+export MCMCSampleIDVector
 
-TaggedMCMCSampleVector(chain::MCMCChain) =
-    TaggedMCMCSampleVector(MCMCSampleVector(chain), Vector{Int64}(), MCMCChainInfoVector())
-
-
-Base.size(xs::TaggedMCMCSampleVector) = size(xs.sampleno)
-
-Base.getindex(xs::TaggedMCMCSampleVector{S}, i::Integer) where {S} =
-    TaggedMCMCSample{S}(xs.sample[i], xs.sampleno[i], xs.chaininfo[i])
-
-# XXXXXXXXXXXXx
+MCMCSampleIDVector() =
+    MCMCSampleIDVector(Vector{Int32}(), Vector{Int32}(), Vector{Int64}())
 
 
-function Base.push!(xs::TaggedMCMCSampleVector, x::TaggedMCMCSample)
-    push!(xs.sample, x.sample)
+Base.size(xs::MCMCSampleIDVector) = size(xs.chainid)
+
+Base.getindex(xs::MCMCSampleIDVector, i::Integer)  =
+    MCMCSampleID(xs.chainid[i], xs.chaincycle[i], xs.sampleno[i])
+
+function Base.push!(xs::MCMCSampleIDVector, x::MCMCSampleID)
+    push!(xs.chainid, x.chainid)
+    push!(xs.chaincycle, x.chaincycle)
     push!(xs.sampleno, x.sampleno)
-    push!(xs.chaininfo, x.chaininfo)
     xs
 end
 
+Base.push!(xs::MCMCSampleIDVector, chain::MCMCChain) =
+    push!(xs, MCMCSampleID(chain))
 
-
-function Base.push!(xs::TaggedMCMCSampleVector, chain::MCMCChain)
-    push!(xs.sample, x.sample)
-    push!(xs.sampleno, current_sampleno(chain ))
-    push!(xs.chaininfo, x.chaininfo)
-    chain
+function Base.append!(A::MCMCSampleIDVector, B::MCMCSampleIDVector)
+    append!(A.chainid, B.chainid)
+    append!(A.chaincycle, B.chaincycle)
+    append!(A.sampleno, B.sampleno)
+    A
 end
-
-
-mcmc_callback(sv::TaggedMCMCSampleVector, args...) = MCMCPushCallback(sv, args...)
-
-
-=#
