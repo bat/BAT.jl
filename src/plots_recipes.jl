60705a901419fad53beea03359aaa77ce3b2e891
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-01 13:56:47 +0200

    improve plotrecipe

--- src/plots_recipes.jl
+++ src/plots_recipes.jl
@@ -7,12 +7,6 @@ using RecipesBase
     acc = find(x -> x > 0, samples.weight)
     rej = find(x -> x <= 0, samples.weight)
 
-    bounds = target.bounds
-    vhi = bounds.vol.hi; vlo = bounds.vol.lo
-    bext = 0.15 * (vhi - vlo)
-    xlims = (vlo[1] - bext[1], vhi[1] + bext[1])
-    ylims = (vlo[2] - bext[2], vhi[2] + bext[2])
-
     base_markersize = 2
 
     @series begin
@@ -20,8 +14,6 @@ using RecipesBase
         label := isempty(rej) ? "samples" : "accepted"
         markersize := base_markersize * sqrt.(samples.weight[acc])
         markerstrokewidth := 0
-        xlims := xlims
-        ylims := ylims
         color := :green
         (samples.params[1, acc], samples.params[2, acc])
     end
@@ -32,25 +24,31 @@ using RecipesBase
             label := "rejected"
             markersize := base_markersize
             markerstrokewidth := 0
-            xlims := xlims
-            ylims := ylims
             color := :red
             (samples.params[1, rej], samples.params[2, rej])
         end
     end
 
-    bounds_rect_X = [vlo[1],vhi[1],vhi[1],vlo[1],vlo[1]]
-    bounds_rect_Y = [vlo[2],vlo[2],vhi[2],vhi[2],vlo[2]]
+    bounds = target.bounds
 
-    @series begin
-        seriestype := :path
-        label := "bounds"
-        linewidth := 3
-        linecolor := :violet
-        xlims := xlims
-        ylims := ylims
-        (bounds_rect_X, bounds_rect_Y)
+    if bounds isa HyperRectBounds
+        vhi = bounds.vol.hi; vlo = bounds.vol.lo
+        bext = 0.1 * (vhi - vlo)
+        xlims = (vlo[1] - bext[1], vhi[1] + bext[1])
+        ylims = (vlo[2] - bext[2], vhi[2] + bext[2])
+        bounds_rect_X = [vlo[1],vhi[1],vhi[1],vlo[1],vlo[1]]
+        bounds_rect_Y = [vlo[2],vlo[2],vhi[2],vhi[2],vlo[2]]
+
+        @series begin
+            seriestype := :path
+            label := "bounds"
+            linewidth := 3
+            linecolor := :violet
+            xlims --> xlims
+            ylims --> ylims
+            (bounds_rect_X, bounds_rect_Y)
+        end
     end
 
-    ()
+    nothing
 end
