013861dc9e7dad5aab2052c11d9d1d852729eaf0
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-17 22:21:21 +0200

    Use Compat.Test for Julia v0.7 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package BAT" begin
+import Compat.Test
+Test.@testset "Package BAT" begin
     include("distributions.jl")
     include("spatialvolume.jl")    
     include("parambounds.jl")
