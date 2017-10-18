b534f8a6530056ee9fb6c255acb9c5947edf0b78
Tobias Pielok <tpielok@mpg.mpp.de>
2017-10-18 13:21:22 +0000

    added test file

---
+++ test/target_density.jl
@@ -0,0 +1,14 @@
+# This file is a part of BAT.jl, licensed under the MIT License (MIT).
+
+using BAT
+using Base.Test
+
+using Distributions, PDMats, StatsBase
+
+
+@testset "target_density" begin
+    @testset "target_logval" begin
+        econtext = ExecContext()
+        
+    end
+end
