b829fed1bdbc6f927a806f98b91edb1e388e315d
Tobias Pielok <tpielok@mpg.mpp.de>
2017-10-19 11:22:30 +0000

    tests for target_density

--- test/target_density.jl
+++ test/target_density.jl
@@ -5,10 +5,42 @@ using Base.Test
 
 using Distributions, PDMats, StatsBase
 
+struct atd_wrap <: AbstractTargetDensity end
 
 @testset "target_density" begin
+    mvec = [-0.3, 0.3]
+    cmat = [1.0 1.5; 1.5 4.0]
+    Σ = @inferred PDMat(cmat)
+    mvnorm = @inferred  MvNormal(mvec, Σ)
+    tdensity = @inferred MvDistTargetDensity(mvnorm)
+
+    econtext = ExecContext()
+
+    params = [0.0 -0.3; 0.0 0.3]
+      
+    @testset "target_logval!" begin
+        r = zeros(Float64, 2)
+
+        target_logval!(r, tdensity, params, econtext)
+        @test r ≈ logpdf(mvnorm, params)
+    end
+
     @testset "target_logval" begin
-        econtext = ExecContext()
-        
+        @test target_logval(tdensity, params[:, 1], econtext) ≈ logpdf(mvnorm, params[:, 1])
+    end
+
+    @testset "exec_capabilities" begin
+        td = atd_wrap()
+        ecap = @inferred BAT.exec_capabilities(target_logval!, td, params)
+        @test ecap.nthreads == 0
+        @test ecap.threadsafe == false
+        @test ecap.nprocs == 0
+        @test ecap.remotesafe == true
+
+        ecap = @inferred BAT.exec_capabilities(target_logval, td, params[:, 1])
+        @test ecap.nthreads == 0
+        @test ecap.threadsafe == false
+        @test ecap.nprocs == 0
+        @test ecap.remotesafe == true
     end
 end
