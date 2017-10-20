068a706ab5124c0a5d3b1aaa78f007a9d1e50ce9
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-20 16:28:56 +0200

    STASH plotting recipes

--- src/plots_recipes.jl
+++ src/plots_recipes.jl
@@ -3,51 +3,69 @@
 
 # ToDo: Modularize
 
-@recipe function f(samples::DensitySampleVector, parsel::NTuple{2,Integer} = (1,2))
+@recipe function f(samples::DensitySampleVector, parsel::NTuple{2,Integer})
     pi_x, pi_y = parsel
 
     acc = find(x -> x > 0, samples.weight)
     rej = find(x -> x <= 0, samples.weight)
 
     base_markersize = get(d, :markersize, 1.5)
+    seriestype = get(d, :seriestype, :scatter)
 
     plot_bounds = get(d, :bounds, true)
     delete!(d, :bounds)
 
-    color = get(d, :seriescolor, :green)
-    label = get(d, :label, isempty(rej) ? "samples" : "accepted")
+    if seriestype == :scatter
+        color = get(d, :seriescolor, :green)
+        label = get(d, :label, isempty(rej) ? "samples" : "accepted")
 
-    @series begin
-        seriestype := :scatter
-        label := label
-        markersize := base_markersize * sqrt.(samples.weight[acc])
-        markerstrokewidth := 0
-        color := color
-        (samples.params[pi_x, acc], samples.params[pi_y, acc])
-    end
-
-    if !isempty(rej)
         @series begin
             seriestype := :scatter
-            label := "rejected"
-            markersize := base_markersize
+            label := label
+            markersize := base_markersize * sqrt.(samples.weight[acc])
             markerstrokewidth := 0
-            color := :red
-            (samples.params[pi_x, rej], samples.params[pi_y, rej])
+            color := color
+            xlabel --> "\$\\theta_$(pi_x)\$"
+            ylabel --> "\$\\theta_$(pi_y)\$"
+            (samples.params[pi_x, acc], samples.params[pi_y, acc])
         end
+
+        if !isempty(rej)
+            @series begin
+                seriestype := :scatter
+                label := "rejected"
+                markersize := base_markersize
+                markerstrokewidth := 0
+                color := :red
+                (samples.params[pi_x, rej], samples.params[pi_y, rej])
+            end
+        end
+    elseif seriestype == :histogram2d
+        @series begin
+            seriestype := :histogram2d
+            label --> "samples"
+            xlabel --> "\$\\theta_$(pi_x)\$"
+            ylabel --> "\$\\theta_$(pi_y)\$"
+            weights := samples.weight
+            (samples.params[pi_x, acc], samples.params[pi_y, acc])
+        end
+    else
+        error("seriestype $seriestype not supported")
     end
 
     nothing
 end
 
 
-@recipe function f(bounds::HyperRectBounds)
+@recipe function f(bounds::HyperRectBounds, parsel::NTuple{2,Integer})
+    pi_x, pi_y = parsel
+
     vhi = bounds.vol.hi; vlo = bounds.vol.lo
     bext = 0.1 * (vhi - vlo)
-    xlims = (vlo[1] - bext[1], vhi[1] + bext[1])
-    ylims = (vlo[2] - bext[2], vhi[2] + bext[2])
-    bounds_rect_X = [vlo[1],vhi[1],vhi[1],vlo[1],vlo[1]]
-    bounds_rect_Y = [vlo[2],vlo[2],vhi[2],vhi[2],vlo[2]]
+    xlims = (vlo[pi_x] - bext[pi_x], vhi[pi_x] + bext[pi_x])
+    ylims = (vlo[pi_y] - bext[pi_y], vhi[pi_y] + bext[pi_y])
+    bounds_rect_X = [vlo[pi_x],vhi[pi_x],vhi[pi_x],vlo[pi_x],vlo[pi_x]]
+    bounds_rect_Y = [vlo[pi_y],vlo[pi_y],vhi[pi_y],vhi[pi_y],vlo[pi_y]]
 
     @series begin
         seriestype := :path
