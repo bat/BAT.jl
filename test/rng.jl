5e911b099656be47986fd9f52f5f1c18b21d06b2
Tobias Pielok <tpielok@mpp.mpg.de>
2017-12-22 18:31:38 +0100

    tests for reset_rng and mersenne

--- test/rng.jl
+++ test/rng.jl
@@ -4,6 +4,11 @@ using BAT
 using Compat.Test
 
 @testset "rng" begin
+    @testset "AbstractRNGSeed" begin
+        philox = @inferred AbstractRNGSeed()
+        @test typeof(philox) <: Philox4xSeed
+    end
+
     @testset "Philox4xSeed" begin
         philox = @inferred Philox4xSeed()
         @test typeof(philox) <: AbstractRNGSeed
@@ -25,5 +30,20 @@ using Compat.Test
         res = @inferred tf4x()
         @test res.key1 == UInt64(2) && res.key2 == UInt64(3) &&
               res.key3 == UInt64(4) && res.key4 == UInt64(5)
+    end
+
+    @testset "reset_rng_counters!" begin
+        philoxS = @inferred Philox4xSeed()
+        philox = @inferred philoxS()
+        BAT.reset_rng_counters!(philox, (1, 2, 3))
+        @test philox.ctr4 == 1 && philox.ctr3 == 2 && philox.ctr2 == 3
+        BAT.reset_rng_counters!(philox, 4, 5, 6)
+        @test philox.ctr4 == 4 && philox.ctr3 == 5 && philox.ctr2 == 6
+    end
+
+    @testset "MersenneTwisterSeed" begin
+        mertwS = @inferred MersenneTwisterSeed()
+        @test typeof(mertwS) <: AbstractRNGSeed
+        @test typeof(@inferred mertwS()) <: MersenneTwister
     end
 end
