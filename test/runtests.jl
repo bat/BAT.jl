3f3fcf00c09bb6718dc2e432ff63a59effdf4710
Tobias Pielok <tpielok@mpp.mpg.de>
2017-12-28 21:37:39 +0100

    Pull request/a65131eb (#37)
    Add tests for BATDataVector

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@
 import Compat.Test
 Test.@testset "Package BAT" begin
     include("mcmc_rand.jl")
+    include("data_vector.jl")
     include("rng.jl")
     include("density.jl")
     include("distributions.jl")
