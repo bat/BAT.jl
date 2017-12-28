a65131eb72d6046cb4978895a23623d67d8df867
Tobias Pielok <tpielok@mpp.mpg.de>
2017-12-28 16:51:05 +0100

    tests for data_vector written

--- test/data_vector.jl
+++ test/data_vector.jl
@@ -3,7 +3,25 @@
 using BAT
 using Compat.Test
 
+struct dv_test{T} <: BAT.BATDataVector{T}
+    data::Vector{T}
+end
+
+Base.append!(x::dv_test, y::dv_test) = append!(x.data, y.data)
+
 @testset "data_vector" begin
-    @testset "merge!" begin
+
+    @testset "data_vector" begin
+        x = @inferred dv_test{Float64}(Float64(1):Float64(3))
+        @test typeof(x) <: DenseVector{Float64}
+    end
+
+    @testset "merge" begin
+        x = @inferred dv_test{Float64}(Float64(1):Float64(3))
+        y = @inferred dv_test{Float64}(Float64(4):Float64(6))
+        z = @inferred dv_test{Float64}(Float64(7):Float64(9))
+        @test merge(x, y, z).data == collect(Float64(1):Float64(9))
+        merge!(x, y, z)
+        @test x.data == collect(Float64(1):Float64(9))
     end
 end
