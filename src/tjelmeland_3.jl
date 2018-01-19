44a6b7101037700ed099c9e8324d158fefb390ad
Unknown <lolian.shtembari@tum.de>
2018-01-19 10:30:13 +0100

    Working on mutating functions

--- src/tjelmeland_3.jl
+++ src/tjelmeland_3.jl
@@ -1,101 +1,137 @@
-# This file is a part of BAT.jl, licensed under the MIT License (MIT).
-
-# similar to mh_sampler.jl
-
-struct MultipleMetropolisHastings{
-    Q<:ProposalDistSpec,
-    W<:Real,
-    WS<:MHWeightingScheme{W},
-    IT<:Integer
-} <: MCMCAlgorithm{AcceptRejectState}
-    q::Q
-    weighting_scheme::WS # TODO do we need a separate hierarchy? Probably yes.
-    m::IT # m=1 is the ordinary mh_sampler
-end
-
-struct MultipleChainState{
-    IT<:Integer
-}
-    κ::IT
-    current::Vector # TODO not typesafe
-    proposed::Matrix # TODO not typesafe
-end
-
-doc"""
-    transition_matrix2(targetvalues::Vector, κ::Int)
-
-Compute the transition matrix (transition alternative 2 in Tjelmeland (2005))
-and return the `κ`-th row. `proposedvalues` has the `m+1` proposed points, the
-current point is the κ-th, and 'pl' computes the p_l values needed to contruct
-the transition function T1.
-"""
-function diag_inspection(row::Vector)
-    row = row ./ sum(row)
-    diag = Float64[]
-    index = Int32[]
-    for i = 1:length(row)
-        if row[i] != 0
-            push!(index, i)
-            push!(diag, row[i])
-        end
-    end
-    return index, diag
-end
-
-function outside_submatrix(row::Vector, index::Vector)
-    outside = Float64[]
-    for i in index
-        out::Float64 = 0.0
-        for j in index
-            out += row[j]
-        end
-        out = 1 - out
-        push!(outside, out)
-    end
-    return outside
-end
-
-function update!(row::Vector, diag::Vector, outside::Vector, index::Vector, u::Float64, κ::Int)
-    diag = 1 - outside - u * (1 - outside - diag)
-    for i in index
-        if i == κ
-            row[i] = diag[i]
-        else
-            row[i] *= u
-        end
-    end
-    for i = 1:length(diag)
-        if diag[i] == 0
-            splice!(diag,i)
-            splice!(outside,i)
-            splice!(index,i)
-        end
-    end
-end
-
-function mini_u(index::Vector, diag::Vector, outside::Vector)
+function min_u(mat::SubArray)
     u = typemax(Float64)
-    for i = 1:length(diag)
-        u_test::Float64 = (1 - outside[i]) / (1 - outside[i] - diag[i])
+    for i = 1:size(mat)[1]
+        summa = sum(mat[i,:])
+        u_test::Float64 = summa/(summa - mat[i,i])
         if u_test < u
             u = u_test
         end
     end
+    println("the minimal is ", u,"\n")
     return u
 end
 
-function T2(row::Vector, κ::Int)
-
-    if row[κ] == 0
-        return row
+function update_submat!(mat::SubArray, u::Float64)
+    for i = 1:size(mat)[1]
+        mat[i,i] = 1 - u * (sum(mat[i,:]) - mat[i,i]) - (1 - sum(mat[i,:]))
     end
-
-    index, diag = diag_inspection(row)
-    outside_submatrix(row, index)
-
-    while in(κ, index) == true
-        u = mini_u(index, diag, outside)
-        update!(row, diag, outside, index, u, κ)
+    mat = mat .* u
+    for i = 1:size(mat)[1]
+        mat[i,i] /= u
     end
+    println("ssubmat before reshaping is ", mat,"\n")
+end
+
+function update_row!(row::Vector, submat_row::Vector, index::Vector)
+    for (i,a) in zip(index,collect(1:1:length(submat_row)))
+        row[i] = submat_row[a]
+    end
+    println("the row now is ", row,"\n")
     return row
 end
+
+function update_index_and_reshape_submat(index::Vector, mat::SubArray)
+    ind_real = Int[]
+    #ind = Int[]
+    for i = 1:size(mat)[1]
+        if mat[i,i] != 0
+            push!(ind_real, index[i])
+            #push!(ind, i)
+        end
+    end
+    #mat2 = zeros(Float64, length(ind), length(ind))
+    #for (i,a) in zip(collect(1:1:length(ind)),ind)
+    #    for (j,b) in zip(collect(1:1:length(ind)),ind)
+    #        mat2[i,j] = mat[a,b]
+    #    end
+    #end
+    return ind_real
+end
+
+
+function T23(row::Vector, κ::Int)
+    row = row ./ sum(row)
+
+    index = Int[]
+    pos_in_submat::Int64 = 0
+    for i = 1:length(row)
+        if row[i] != 0
+            push!(index, i)
+        end
+    if in(κ, index)
+        pos = find(index .== κ)[1]
+    end
+    println("index is ",index)
+    submat = zeros(Float64, length(index), length(index))
+
+    for i = 1:length(index)
+        for j = 1:length(index)
+            submat[i,j] = row[index[j]]
+        end
+    end
+    submat_view = view(submat, :, :)
+    println("****submat is ",submat_view)
+    passo = 1
+    while length(index) > 1
+        print(" passo is ",passo,"\n")
+        if ∉(κ, index)
+            print(" ------------------ the index now is ", index,"\n")
+            println(" ---------------- we broke ;) \n")
+            break
+        end
+        u = min_u(submat_view)
+        update_submat!(submat_view, u)
+        println(" e momomom ", submat_view)
+        update_row!(row, submat_view[find(index .== κ)[1], :], index::Vector)
+        #index = update_index_and_reshape_submat(index, submat_view)
+        for i = 1:length(index)
+            if submat_view[i,i] == 0
+                index[i] = 0
+            end
+        end
+        j = 1
+        while j <= length(index)
+            if index[j] == 0
+                splice!(index,j)
+                j -= 1
+            end
+            j += 1
+        end
+    end
+        println("after update index is ",index)
+        println("ricordiamoci di submat vero ", submat)
+        submat_view = view(submat, index, index)
+        println(" fatto \n")
+        println(" submat is ",submat_view,"\n")
+        passo += 1
+    end
+    println(" finito ")
+    return row
+end
+
+input = [0.2, 0.5, 0.3]
+κ = 3
+result_1 =[0.25, 0.75, 0.0]
+result_2=T23(input, κ)
+
+println(result_2-result_1)
+
+    #input = [288., 64., 135.] / 487.::Float64
+    #κ = 3
+    #result_1 =[359., 64, 0.] / 423.::Float64
+    #result_2=T23(input, κ)
+
+    #print(result_2-result_1)
+    #print(T23(input, κ) ≈ result_1)
+
+# a = [1,2,3,4,5,6,7,8,9]
+#
+# for i = 1:length(a)
+#     print(i)
+#     splice!(a,i)
+#     i -= 1
+#     if i == length(a)
+#         println("wewweweeferf")
+#         break
+#     end
+# end
