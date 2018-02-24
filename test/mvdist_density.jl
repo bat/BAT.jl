e783716d7899b8d47d8c94b6ccd09a171f086b19
Tobias Pielok <tpielok@mpp.mpg.de>
2018-02-24 11:06:22 +0100

    mvdist_density test file added

---
+++ test/mvdist_density.jl
@@ -0,0 +1,9 @@
+# This file is a part of BAT.jl, licensed under the MIT License (MIT).
+
+using BAT
+using Compat.Test
+
+@testset "mvdist_density" begin
+    @testset "mvdist_density" begin
+    end
+end
