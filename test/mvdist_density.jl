4bbb2c05877559e1ff29b73d925532034fe0432e
Tobias Pielok <tpielok@mpp.mpg.de>
2018-02-26 11:31:59 +0100

    tests for mvdist_density

--- test/mvdist_density.jl
+++ test/mvdist_density.jl
@@ -2,8 +2,40 @@
 
 using BAT
 using Compat.Test
+using Distributions
+using PDMats
 
 @testset "mvdist_density" begin
+    mvt = @inferred MvTDist(1.5, PDMat([2.0 1.0; 1.0 3.0]))
+    mvdd = @inferred MvDistDensity(mvt)
+
     @testset "mvdist_density" begin
+        @test typeof(mvdd) <: AbstractDensity
+        @test parent(mvdd) == mvt
+        @test nparams(mvdd) == 2
+
+        @test typeof(sampler(mvdd)) <: BATMvTDistSampler
+    end
+
+    @testset "unsafe_density_logval" begin
+        BAT.unsafe_density_logval(mvdd, [0.0, 0.0]) ≈
+            -2.64259602
+        param = zeros(2)
+        ec = @inferred BAT.exec_capabilities(BAT.unsafe_density_logval, mvdd, param)
+        @test ec.nthreads == 0
+        @test ec.threadsafe == true
+        @test ec.nprocs == 0
+        @test ec.remotesafe == true
+
+        ret = Array{Float64}(2)
+        param = [0.0 0.5; 0.0 -0.5]
+        BAT.unsafe_density_logval!(ret, mvdd, param)
+        @test ret ≈ [-2.64259602, -3.00960695]
+        ec = @inferred BAT.exec_capabilities(BAT.unsafe_density_logval!, ret,
+            mvdd, param)
+        @test ec.nthreads == 0
+        @test ec.threadsafe == true
+        @test ec.nprocs == 0
+        @test ec.remotesafe == true
     end
 end
