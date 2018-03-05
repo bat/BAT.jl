0da4d28fc6b48198ae2cc2756f41abc26f8dcc80
Tobias Pielok <tpielok@mpp.mpg.de>
2018-03-05 16:51:49 +0100

    Create testfiles for shims and util

---
+++ test/shims.jl
@@ -0,0 +1,11 @@
+# This file is a part of BAT.jl, licensed under the MIT License (MIT).
+
+using BAT
+using Compat.Test
+
+@testset "shims" begin
+    @testset "_iszero" begin
+        
+    end
+end
+
