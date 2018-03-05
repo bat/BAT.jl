caed31512e928b79f73205be79c1123276a6a00a
Tobias Pielok <tpielok@mpp.mpg.de>
2018-03-05 17:05:57 +0100

    Add tests for shims

--- test/shims.jl
+++ test/shims.jl
@@ -3,9 +3,14 @@
 using BAT
 using Compat.Test
 
+using Distributions
+
 @testset "shims" begin
     @testset "_iszero" begin
-        
+        @test BAT._iszero(zero(Float64))
+        @test BAT._iszero(zeros(Float32,4,4))
+        @test !BAT._iszero(ones(Float32,4,4))
+        @test BAT._iszero(Distributions.ZeroVector(Float64, 4))
     end
 end
 
