42e1a78d20342807d1812677fba454d44e1a1f74
Oliver Schulz <oschulz@mpp.mpg.de>
2018-05-17 23:26:19 +0200

    Add BATMvTDistSampler

--- src/distributions.jl
+++ src/distributions.jl
@@ -43,6 +43,8 @@ issymmetric_around_origin(d::Gamma) = false
 
 issymmetric_around_origin(d::Chisq) = false
 
+issymmetric_around_origin(d::TDist) = true
+
 issymmetric_around_origin(d::MvNormal) = _iszero(d.μ)
 
 issymmetric_around_origin(d::Distributions.GenericMvTDist) = d.zeromean
@@ -169,6 +171,38 @@ bat_sampler(d::Chisq) = BATChisqSampler(d)
 
 
 
+_tdist_scaling_factor(rng::AbstractRNG, x::AbstractVector, df::Real) = sqrt(df / rand(rng, bat_sampler(Chisq(df))))
+
+function _tdist_scaling_factor(rng::AbstractRNG, x::AbstractMatrix, df::Real)
+    scaling_factor = similar(x, 1, size(x, 2)) # TODO: Avoid memory allocation
+    rand!(rng, bat_sampler(Chisq(df)), scaling_factor)
+    scaling_factor .= sqrt.(df ./ scaling_factor)
+end
+
+
+
+export BATTDistSampler
+
+struct BATTDistSampler{T<:AbstractFloat} <: BATSampler{Univariate,Continuous}
+    d::TDist{T}
+end
+
+Base.eltype(s::BATTDistSampler{T}) where T = T
+
+function Base.rand(rng::AbstractRNG, s::BATTDistSampler{T}) where T
+    ndof = dof(s.d)
+    x = randn(rng, T)
+    if ndof == Inf
+        convert(T, x)
+    else
+        convert(T, x * sqrt(ndof / rand(rng, bat_sampler(Chisq(ndof)))))
+    end
+end
+
+bat_sampler(d::TDist) = BATTDistSampler(d)
+
+
+
 export BATMvTDistSampler
 
 struct BATMvTDistSampler{T<:Distributions.GenericMvTDist} <: BATSampler{Multivariate,Continuous}
@@ -181,16 +215,6 @@ Base.length(s::BATMvTDistSampler) = length(s.d)
 Base.eltype(s::BATMvTDistSampler) = eltype(s.d.Σ)
 
 
-
-_tdist_scaling_factor(rng::AbstractRNG, x::AbstractVector, df::Real) = sqrt(df / rand(rng, bat_sampler(Chisq(df))))
-
-function _tdist_scaling_factor(rng::AbstractRNG, x::AbstractMatrix, df::Real)
-    scaling_factor = similar(x, 1, size(x, 2)) # TODO: Avoid memory allocation
-    rand!(rng, bat_sampler(Chisq(df)), scaling_factor)
-    scaling_factor .= sqrt.(df ./ scaling_factor)
-end
-
-
 function Base.rand!(rng::AbstractRNG, s::BATMvTDistSampler, x::StridedVecOrMat{T}) where {T<:Real}
     d = s.d
 
@@ -199,7 +223,6 @@ function Base.rand!(rng::AbstractRNG, s::BATMvTDistSampler, x::StridedVecOrMat{T
     broadcast!(muladd, x, x, scaling_factor, d.μ)
     x
 end
-
 
 
 bat_sampler(d::Distributions.GenericMvTDist) = BATMvTDistSampler(d)
