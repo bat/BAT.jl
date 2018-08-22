553b0ad94770c9bb6ca124390325ea51f4b0c97a
Tobias Pielok <t.pielok@tum.de>
2018-08-22 14:06:45 +0200

    Implement rvalue according to brooks-gelman

--- src/mcmc_convergence.jl
+++ src/mcmc_convergence.jl
@@ -26,12 +26,57 @@ doc"""
 
 Gelman-Rubin $R^2$ for all parameters.
 """
-function gr_Rsqr(stats::AbstractVector{<:MCMCBasicStats})
+function gr_Rsqr(stats::AbstractVector{<:MCMCBasicStats})    
     m = nparams(first(stats))
     W = mean([cs.param_stats.cov[i,i] for cs in stats, i in 1:m], 1)[:]
     B = var([cs.param_stats.mean[i] for cs in stats, i in 1:m], 1)[:]
     (W .+ B) ./ W
 end
+
+
+
+doc"""
+    bg_R_2sqr(stats::AbstractVector{<:MCMCBasicStats})
+
+Brooks-Gelman $R_2^2$ for all parameters.
+If normality is assumed, 'corrected' should be set to true to account for the sampling variability.
+"""
+function bg_R_2sqr(stats::AbstractVector{<:MCMCBasicStats},
+                  corrected::Bool=true)
+    
+    p = nparams(first(stats))
+    m = length(stats)
+    n = Float64(mean(nsamples.(stats)))
+    
+    σ_W = var([cs.param_stats.cov[i,i] for cs in stats, i in 1:p], 1)[:]
+    B  = var([cs.param_stats.mean[i] for cs in stats, i in 1:p], 1)[:]
+    W = mean([cs.param_stats.cov[i,i] for cs in stats, i in 1:p], 1)[:]
+
+    σ_sq = m * (n - 1) / (m*n - 1) * W + n * (m - 1) / (m*n - 1) * B
+    
+    R_unc = σ_sq ./ W
+
+    if corrected == false
+        return R_unc
+    end
+
+    σ_ij = [cs.param_stats.cov[i,i] for cs in stats, i in 1:p]
+    x_ij = [cs.param_stats.mean[i] for cs in stats, i in 1:p]
+    
+    cov_σx = [cov(σ_ij[:,j], x_ij[:,j]) for j in 1:p]
+    cov_σx_sq = [cov(σ_ij[:,j], x_ij[:,j].^2) for j in 1:p]
+    
+    N = (n-1)/n
+    M = (m-1)/m
+    V = N*σ_sq + M*B
+
+    σ_V = N^2/m*σ_W + 2*M/(m-1)*B.^2 + 2*M*N/m*(cov_σx_sq - 2*B.*cov_σx)
+    d = 2*V.^2./σ_V
+
+    R_unc.*(d+3)./(d+1)
+end
+
+
 
 
 
@@ -56,7 +101,7 @@ end
 
 
 function check_convergence(ct::GRConvergence, stats::AbstractVector{<:MCMCBasicStats}; ll::LogLevel = LOG_NONE)
-    max_Rsqr = maximum(gr_Rsqr(stats))
+    max_Rsqr = maximum(bg_R_2sqr(stats))
     converged = max_Rsqr <= ct.threshold
     @log_msg ll begin
         success_str = converged ? "have" : "have *not*"
