b92a194f6b54bdfc578e2449d5e354375bad9a81
Cornelius-G <Cornelius-G@users.noreply.github.com>
2019-02-05 13:43:37 +0100

    updated plotting.jl

--- src/plotting/plotting.jl
+++ src/plotting/plotting.jl
@@ -3,6 +3,7 @@
 const standard_confidence_vals = [0.683, 0.955, 0.997]
 const standard_colors = [:chartreuse2, :yellow, :red]
 
+
 include("recipes_1d_histograms.jl")
 include("recipes_2d_histograms.jl")
 include("recipes_overview.jl")
