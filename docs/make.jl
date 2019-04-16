005f32a139e44de94ae5680f270ae90dbc5b6e69
Oliver Schulz <oschulz@mpp.mpg.de>
2019-04-16 13:28:19 +0200

    Set GR to headless mode for docs generation

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,8 @@ using Documenter
 using Literate
 using BAT
 
+ENV["GKSwstype"] = "nul"
+
 gen_content_dir = joinpath(@__DIR__, "src")
 tutorial_src = joinpath(@__DIR__, "src", "tutorial_lit.jl")
 Literate.markdown(tutorial_src, gen_content_dir, name = "tutorial", documenter = true, credit = true)
