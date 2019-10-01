963b2013d3c1144890f1310644b33b4d4bbe10aa
Oliver Schulz <oschulz@mpp.mpg.de>
2019-10-02 00:17:05 +0200

    Use PositiveFactorizations.Positive in tuning update
    Replace hand-written method that makes covariance matrix posdef

--- src/samplers/mcmc/algorithms/mh_tuner.jl
+++ src/samplers/mcmc/algorithms/mh_tuner.jl
@@ -89,15 +89,8 @@ function tuning_update!(tuner::ProposalCovTuner, chain::MHIterator)
         end
     end
 
-    Σ_new = Matrix(Hermitian(new_Σ_unscal * tuner.scale))
-
-    if !isposdef(Σ_new)
-        ev = convert(Vector{eltype(Σ_new)}, eigen(Σ_new).values) # require real-valued eigenvalues
-        ev_min = minimum(ev)
-        Σ_11 = Σ_new[1,1]
-        Σ_new += max(10 * eps(typeof(ev_min)), -ev_min) * Matrix{eltype(ev)}(I, length(ev), length(ev))
-        Σ_new ./= (Σ_new[1,1] / Σ_11)
-    end
+    Σ_new_raw = new_Σ_unscal * tuner.scale
+    Σ_new = Matrix(cholesky(Positive, Hermitian(Σ_new_raw)))
 
     next_cycle!(chain)
     chain.proposaldist = set_cov(chain.proposaldist, Σ_new)
