0c7a4fcbf64fb606c2a12adefa1dd22dfe84283a
Oliver Schulz <oschulz@mpp.mpg.de>
2019-12-01 14:51:22 +0100

    Enhance set_cov mechanism to support type conversion

--- src/samplers/mcmc/algorithms/mh_tuner.jl
+++ src/samplers/mcmc/algorithms/mh_tuner.jl
@@ -99,8 +99,7 @@ function tuning_update!(tuner::ProposalCovTuner, chain::MHIterator)
         end
     end
 
-    Σ_new_raw = new_Σ_unscal * tuner.scale
-    Σ_new = PDMat(cholesky(Positive, Hermitian(Σ_new_raw)))
+    Σ_new = new_Σ_unscal * tuner.scale
 
     next_cycle!(chain)
     chain.proposaldist = set_cov(chain.proposaldist, Σ_new)
