b26dd1b1aae2a66c5c17ba3cdbc906ddf1139725
Cornelius-G <cornelius.grunwald@tu-dortmund.de>
2020-06-04 16:58:14 +0200

    add function to determine prior bounds

---
+++ src/samplers/importance/prior_bounds.jl
@@ -0,0 +1,19 @@
+function get_bounds(prior::NamedTupleDist)
+    bounds = [get_bounds(d) for d in values(prior)]
+    return vcat(bounds...)
+end
+
+
+function get_bounds(d::Distribution)
+    lo, hi = minimum(d), maximum(d)
+
+    lo == -Inf ? lo = quantile(d, 0.00001) : nothing
+    hi ==  Inf ? hi = quantile(d, 0.99999) : nothing
+
+    return lo, hi
+end
+
+function get_bounds(d::Product)
+    bounds = get_bounds.(d.v)
+    return vcat(bounds...)
+end
