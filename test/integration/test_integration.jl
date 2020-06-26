edcf98847ba62a11f9cbea5822710cef1a442c9d
Scott Hayashi <scott.hayashi@tum.de>
2020-06-27 01:30:02 +0200

    Restructed integration tests to use a single function

--- test/integration/test_integration.jl
+++ test/integration/test_integration.jl
@@ -2,37 +2,22 @@ using Distributions
 using BAT
 using ValueShapes
 using IntervalSets
+using LinearAlgebra: Diagonal, ones
 using Test
 
 @testset "bat_integrate" begin
-    @testset "multimodal cauchy" begin
-        dist = BAT.MultimodalCauchy() 
+    function test_integration(algorithm::IntegrationAlgorithm, title::String, 
+                              dist::Distribution, nsamples::Int=10^5, val_expected::Real=1.0, 
+                              val_rtol::Real=3.1, err_max::Real=0.1)
+        @testset "$title" begin
+            sample = bat_sample(dist, nsamples).result
+            sample_integral = bat_integrate(sample, algorithm).result
 
-        sample = bat_sample(dist, 100000).result
-        sample_integral = bat_integrate(sample).result
-
-        @test isapprox(sample_integral.val, 1, atol=3.1*sample_integral.err)
-        @test sample_integral.err < 0.15
+            @test isapprox(sample_integral.val, val_expected, atol=val_rtol*sample_integral.err)
+            @test sample_integral.err < err_max
+        end
     end
-
-    @testset "MvN" begin
-        dist = MvNormal([1. 0. 0. 0. 0.;
-                         0. 1. 0. 0. 0.;
-                         0. 0. 1. 0. 0.;
-                         0. 0. 0. 1. 0.;
-                         0. 0. 0. 0. 1.])
-        sample = bat_sample(dist, 100000).result
-        sample_integral = bat_integrate(sample).result
-
-        @test isapprox(sample_integral, 1., atol=3.1*sample_integral.err)
-    end
-
-    @testset "funnel" begin
-        dist = BAT.FunnelDistribution()
-
-        sample = bat_sample(dist, 100000).result
-        sample_integral = bat_integrate(sample).result
-
-        @test isapprox(sample_integral.val, 1, atol=3.1*sample_integral.err)
-    end
+    test_integration(AHMIntegration(), "multimodal cauchy", BAT.MultimodalCauchy(), 10^6)
+    test_integration(AHMIntegration(), "funnel distribution", BAT.FunnelDistribution(), 10^6)
+    test_integration(AHMIntegration(), "MvNormal", MvNormal(Diagonal(ones(5))), 10^6)
 end
