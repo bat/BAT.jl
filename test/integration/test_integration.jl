0d27dd0712f0ad0e5188a355a3a8bb5883674ea1
Scott Hayashi <scott.hayashi@tum.de>
2020-06-30 13:26:04 +0200

    Int to Integer in functiion arg of test_integration.jl

--- test/integration/test_integration.jl
+++ test/integration/test_integration.jl
@@ -7,19 +7,13 @@ using Test
 
 @testset "bat_integrate" begin
     function test_integration(algorithm::IntegrationAlgorithm, title::String,
-                              dist::Distribution; nsamples::Int=10^5, val_expected::Real=1.0,
+                              dist::Distribution; nsamples::Integer=10^5, val_expected::Real=1.0,
                               val_rtol::Real=3.5, err_max::Real=0.1)
         println("Using algorithm:", algorithm)
         @testset "$title" begin
             sample = bat_sample(dist, nsamples).result
             sample_integral = bat_integrate(sample, algorithm).result
 
-            println("==========")
-            println(val_rtol)
-            println(sample_integral.err)
-            println(val_rtol*sample_integral.err)
-            println(sample_integral.val)
-            println("==========")
             @test isapprox(sample_integral.val, val_expected, atol=val_rtol*sample_integral.err)
             @test sample_integral.err < err_max
         end
