bef09583834860d4740b32451549d335eb0501d2
Scott Hayashi <scott.hayashi@tum.de>
2020-07-20 02:23:24 +0200

    Changed rand functions for gaussian_shell.jl. Struct now uses AbstractFloat and AbstractVector with known eltype. Moved normalization calculation to constructor.

--- src/distributions/gaussian_shell.jl
+++ src/distributions/gaussian_shell.jl
@@ -17,7 +17,7 @@ Constructors:
 ```julia
 BAT.GaussianShell(r::Real, w::Real, c::Vector, n::Int)
 """
-struct GaussianShell{T<:Real, V<:AbstractVector, F<:Float64} <: ContinuousMultivariateDistribution
+struct GaussianShell{T<:Real, V<:AbstractVector{<:Real}, F<:AbstractFloat} <: ContinuousMultivariateDistribution
     r::T
     w::T
     c::V
@@ -28,8 +28,11 @@ end
 function GaussianShell(;r::Real=5, w::Real=2, n::Integer=2)
     c = zeros(n)
     r,w = promote(r, w)
-    f(x) = x^(n-1)*exp(-(x - r)^2 / (2*w^2))
-    lognorm = log((sqrt(2)*π^((n-1)/2)) / (gamma(n/2)*w)*QuadGK.quadgk(f, 0, r+w*20)[1])
+    radial_integral(ρ) = ρ^(n-1)*exp(-(ρ - r)^2 / (2*w^2))
+    density_norm = sqrt(2*π*w^2)
+    # Normalization for coordinate transform
+    radial_norm = (sqrt(2)*π^((n-1)/2)) / (gamma(n/2)*w)*QuadGK.quadgk(radial_integral, 0, r+w*20)[1]
+    lognorm = log(density_norm) + log(radial_norm)
     GaussianShell(r, w, c, n, lognorm)
 end
 
@@ -42,26 +45,32 @@ function gauss_shell_radial_samples(rng::AbstractRNG, ndims::Integer, r0::Real, 
     AdaptiveRejectionSampling.run_sampler!(rng, sampler, n)
 end
 
-function rand_nball_surf_samples(rng::AbstractRNG, ndims::Integer, n::Integer)
-    A = randn(rng, ndims, n)
-    A ./= sqrt.(sum(A .* A, dims = 1))
+function rand_nball_surf_samples!(rng::AbstractRNG, X::AbstractMatrix)
+    randn!(rng, X)
+    X ./= sqrt.(sum(X .* X, dims = 1))
+    return X
 end
 
 function Distributions.rand(rng::AbstractRNG, d::GaussianShell, n::Int)
-    Distributions._rand!(rng, d, Matrix{eltype(d)}(undef, length(d), n), n)
+    X = Matrix{eltype(d)}(undef, length(d), n)
+    Distributions._rand!(rng, d, X)
 end
 
-function Distributions._rand!(rng::AbstractRNG, d::GaussianShell, X::AbstractMatrix, n::Int)
-    X = rand_nball_surf_samples(rng, d.n, n)
-    R = gauss_shell_radial_samples(rng, d.n, d.r, d.w, n)
-    X .* R'
+function Distributions._rand!(rng::AbstractRNG, d::GaussianShell, X::AbstractMatrix)
+    rand_nball_surf_samples!(rng, X)
+    R = gauss_shell_radial_samples(rng, d.n, d.r, d.w, size(X,2))
+    X .*= R'
+    return X
 end
 
 function Distributions._logpdf(d::GaussianShell, x::AbstractArray)
-    pdf_normalization = -log(sqrt(2*π*(d.w)^2))
     integral_result = -(sqrt(sum((x .- d.c).^2)) - d.r)^2 / (2*d.w^2)
-    result = integral_result + pdf_normalization - d.lognorm
+    result = integral_result - d.lognorm
     return result
+end
+
+function Statistics.cov(d::GaussianShell)
+    cov(nestedview(rand(bat_determ_rng(), sampler(d), 10^5)))
 end
 
 Base.length(d::GaussianShell) = length(d.c)
