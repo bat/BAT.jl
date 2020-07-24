8dca35c6422e3cd3bd8a57f180a36481199d61e8
Cornelius-G <cornelius.grunwald@tu-dortmund.de>
2020-07-24 11:58:24 +0200

    modify example

--- examples/dev-internal/output_examples.jl
+++ examples/dev-internal/output_examples.jl
@@ -1,48 +1,32 @@
-# # BAT.jl plotting tutorial
-
 using BAT
 using Distributions
 using IntervalSets
-using ValueShapes
 
-# ## Generate samples to be plotted
 likelihood = params -> begin
-
-    r1 = logpdf.(
-    MixtureModel(Normal[
-    Normal(-10.0, 1.2),
-    Normal(0.0, 1.8),
-    Normal(10.0, 2.5)], [0.1, 0.3, 0.6]), params.a)
-
-    r2 = logpdf.(
-    MixtureModel(Normal[
-    Normal(-5.0, 2.2),
-    Normal(5.0, 1.5)], [0.3, 0.7]), params.c[1])
-
-    r3 = logpdf.(Normal(2.0, 1.5), params.c[2])
+    r1 = logpdf.(Normal(-10.0, 1.2), params.a)
+    r2 = logpdf.(Normal(-5.0, 0.8), params.c[1])
+    r3 = logpdf.(Normal(12.0, 1.4), params.c[2])
 
     return LogDVal(r1+r2+r3)
 end
 
 prior = BAT.NamedTupleDist(
     a = Normal(-3, 4.5),
-    #b = 0,
+    b = 0,
     c = [-20.0..20.0, -10..10]
 )
 
 posterior = PosteriorDensity(likelihood, prior);
 
-samples, summary, chains = bat_sample(posterior, 10^5, SobolSampler());
+samples, summary, chains = bat_sample(posterior, (10^5, 4), MetropolisHastings());
 display(summary)
 
-
-#nb # ### plain text output (for terminal)
-#nb show(stdout,"text/plain", summary)
-
-# ### write output to txt-file
-io = open("output.txt", "w")
+# write summary to txt-file
+io = open("summary.txt", "w")
 show(io, "text/plain", summary)
 close(io)
+
+
 
 
 # ### write output to html-file
