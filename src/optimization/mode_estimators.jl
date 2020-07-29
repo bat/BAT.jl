f5342a8dccee32eb1f88328b24c24ddba4dc0aa1
Oliver Schulz <oschulz@mpp.mpg.de>
2020-07-29 21:40:49 +0200

    Don't use Plots._auto_binning_nbins, BAT doesn't depend on Plots

--- src/optimization/mode_estimators.jl
+++ src/optimization/mode_estimators.jl
@@ -122,7 +122,7 @@ function bat_marginalmode_impl(samples::DensitySampleVector; nbins::Union{Intege
 
     for param in Base.OneTo(n_params)
         if typeof(nbins) == Symbol
-            number_of_bins = Plots._auto_binning_nbins(nt_samples, param, mode=nbins)
+            number_of_bins = _auto_binning_nbins(nt_samples, param, mode=nbins)
         else
             number_of_bins = nbins
         end
@@ -135,4 +135,41 @@ function bat_marginalmode_impl(samples::DensitySampleVector; nbins::Union{Intege
         push!(marginalmode_params, marginalmode_param[1][1])
     end
     (result = shape(marginalmode_params),)
+end
+
+
+# From Plots.jl, original authors Oliver Schulz and Michael K. Borregaard
+function _auto_binning_nbins(vs::NTuple{N,AbstractVector}, dim::Integer; mode::Symbol = :auto) where N
+    max_bins = 10_000
+    _cl(x) = min(ceil(Int, max(x, one(x))), max_bins)
+    _iqr(v) = (q = quantile(v, 0.75) - quantile(v, 0.25); q > 0 ? q : oftype(q, 1))
+    _span(v) = maximum(v) - minimum(v)
+
+    n_samples = length(LinearIndices(first(vs)))
+
+    # The nd estimator is the key to most automatic binning methods, and is modified for twodimensional histograms to include correlation
+    nd = n_samples^(1/(2+N))
+    nd = N == 2 ? min(n_samples^(1/(2+N)), nd / (1-cor(first(vs), last(vs))^2)^(3//8)) : nd # the >2-dimensional case does not have a nice solution to correlations
+
+    v = vs[dim]
+
+    if mode == :auto
+        mode = :fd
+    end
+
+    if mode == :sqrt  # Square-root choice
+        _cl(sqrt(n_samples))
+    elseif mode == :sturges  # Sturges' formula
+        _cl(log2(n_samples) + 1)
+    elseif mode == :rice  # Rice Rule
+        _cl(2 * nd)
+    elseif mode == :scott  # Scott's normal reference rule
+        _cl(_span(v) / (3.5 * std(v) / nd))
+    elseif mode == :fd  # Freedman–Diaconis rule
+        _cl(_span(v) / (2 * _iqr(v) / nd))
+    elseif mode == :wand
+        _cl(wand_edges(v))  # this makes this function not type stable, but the type instability does not propagate
+    else
+        error("Unknown auto-binning mode $mode")
+    end
 end
