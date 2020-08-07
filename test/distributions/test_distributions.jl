b538a8751765ccf50d0f5e3df09fd613ad36603f
Scott Hayashi <scott.hayashi@tum.de>
2020-08-08 01:28:12 +0200

    Simple statistical tests for multimodal cauchy and funnel distributions

--- test/distributions/test_distributions.jl
+++ test/distributions/test_distributions.jl
@@ -1,8 +1,37 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
+using BAT
+using LinearAlgebra: diag
+using Statistics
+using StatsBase
 using Test
 
 Test.@testset "distributions" begin
-    include("test_distribution_functions.jl")
-    include("test_bat_sampler.jl")
+#   include("test_distribution_functions.jl")
+#   include("test_bat_sampler.jl")
+
+    Test.@testset "multimodal cauchy" begin
+        for i in 2:6
+            μ = randn()
+            σ = rand(0:0.1:2)
+            mmc = BAT.MultimodalCauchy(μ=μ,σ=σ, n=i)
+            @test isequal(mean(mmc), fill(NaN, i))
+            @test isequal(var(mmc), fill(NaN, i))
+            @test isequal(diag(cov(mmc)), fill(NaN, i))
+            ps = StatsBase.params(mmc)
+            @test sort(ps[1]) == sort(vcat([μ, -μ], zeros(i-2)))
+            @test ps[2] == [σ for j in 1:i]
+            @test ps[3] == i
+        end
+    end
+
+    Test.@testset "funnel distribution" begin
+        for i in 2:6
+            a = randn()
+            b = randn()
+            funnel = BAT.FunnelDistribution(a=a, b=b, n=i)
+            @test mean(funnel) == zeros(i)
+            @test StatsBase.params(funnel) == (a, b, i)
+        end
+    end
 end
