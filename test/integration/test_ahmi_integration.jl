0b88689034b9296799d1454158af3d6a3536b3ca
Scott H <scott.hayashi@tum.de>
2020-08-09 10:24:29 +0200

    Adjust AHMIntegration test rtols

--- test/integration/test_ahmi_integration.jl
+++ test/integration/test_ahmi_integration.jl
@@ -19,7 +19,8 @@ using LinearAlgebra: Diagonal, ones
             @test sample_integral.err < err_max
         end
     end
-    test_integration(AHMIntegration(), "funnel distribution", BAT.FunnelDistribution(), val_rtol = 15)
+    test_integration(AHMIntegration(), "funnel distribution", BAT.FunnelDistribution(), val_rtol = 14)
     test_integration(AHMIntegration(), "multimodal cauchy", BAT.MultimodalCauchy(), val_rtol = 35)
+    test_integration(AHMIntegration(), "Gaussian shell", BAT.GaussianShell(), val_rtol = 6.2)
     test_integration(AHMIntegration(), "MvNormal", MvNormal(Diagonal(ones(5))), val_rtol = 15)
 end
