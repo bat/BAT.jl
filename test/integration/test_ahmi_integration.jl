6a8e834ca92e56bb865151ca4128144b01875170
Scott Hayashi <scott.hayashi@tum.de>
2020-11-27 20:58:21 +0100

    Moved tests into effective_sample_size.jl

--- test/integration/test_ahmi_integration.jl
+++ test/integration/test_ahmi_integration.jl
@@ -23,26 +23,4 @@ using LinearAlgebra: Diagonal, ones
     test_integration(AHMIntegration(), "multimodal cauchy", BAT.MultimodalCauchy(), val_rtol = 35)
     test_integration(AHMIntegration(), "Gaussian shell", BAT.GaussianShell(), val_rtol = 6.2)
     test_integration(AHMIntegration(), "MvNormal", MvNormal(Diagonal(ones(5))), val_rtol = 15)
-
-    @testset "ahmi_integration_defaults" begin
-        bsample = @inferred(bat_sample(product_distribution([Normal() for i in 1:3]), 10^4)).result
-        @test isapprox(bat_integrate(bsample).result.val, 1.0, rtol=15)
-        eff_sample_size_dsv = @inferred(bat_eff_sample_size(bsample)).result
-        eff_sample_size_aosa = @inferred(bat_eff_sample_size(bsample.v)).result
-
-        [bsample.weight[i] = 0 for i in eachindex(bsample.weight)]
-        bsample.weight[1] = 1
-        eff_sample_size_dsv_single_weight = @inferred(bat_eff_sample_size(bsample)).result
-
-        for i in eachindex(@inferred(bat_eff_sample_size(bsample)).result)
-            @test 8000 <= eff_sample_size_dsv[i] <= 10^4
-            @test eff_sample_size_dsv[i] == eff_sample_size_aosa[i]
-        end
-
-        for i in eachindex(eff_sample_size_dsv_single_weight)
-            @test @inferred(isapprox(1, eff_sample_size_dsv_single_weight[i]))
-        end
-
-    end
-
 end
--- test/statistics/test_effective_sample_size.jl
+++ test/statistics/test_effective_sample_size.jl
@@ -1,6 +1,7 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
 using BAT
+using Distributions
 using Test
 
 using Statistics
@@ -19,6 +20,24 @@ using StableRNGs
     end
 
     v1 = flatview(v)[1, :]
+
+    bsample = @inferred(bat_sample(product_distribution([Normal() for i in 1:3]), 10^4)).result
+    @test isapprox(bat_integrate(bsample).result.val, 1.0, rtol=15)
+    eff_sample_size_dsample_vec = @inferred(bat_eff_sample_size(bsample)).result
+    eff_sample_size_arr_of_sim_arr = @inferred(bat_eff_sample_size(bsample.v)).result
+
+    [bsample.weight[i] = 0 for i in eachindex(bsample.weight)]
+    bsample.weight[1] = 1
+    eff_sample_size_dsample_vec_single_weight = @inferred(bat_eff_sample_size(bsample)).result
+
+    for i in eachindex(@inferred(bat_eff_sample_size(bsample)).result)
+        @test 8000 <= eff_sample_size_dsample_vec[i] <= 10^4
+        @test eff_sample_size_dsample_vec[i] == eff_sample_size_arr_of_sim_arr[i]
+    end
+
+    for i in eachindex(eff_sample_size_dsample_vec_single_weight)
+        @test @inferred(isapprox(1, eff_sample_size_dsample_vec_single_weight[i]))
+    end
 
     @testset "BAT.bat_integrated_autocorr_len" begin
         @test @inferred(bat_integrated_autocorr_len(v1, GeyerAutocorLen())).result ≈ 52.2404651916953
