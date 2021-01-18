633dbc9b62ac3c11168ec84517fdda03232efb83
Oliver Schulz <oschulz@mpp.mpg.de>
2021-01-18 01:14:46 +0100

    Add tests for HDF5 bat_write and bat_read

--- test/io/test_hdf5.jl
+++ test/io/test_hdf5.jl
@@ -3,8 +3,15 @@
 using BAT
 using Test
 
-#import HDF5
+import HDF5
 
 
 @testset "hdf5" begin
+    mktempdir() do tmp_datadir
+        results_filename = joinpath(tmp_datadir, "results.hdf5")
+        samples = bat_sample(BAT.example_posterior(), MCMCSampling(nsteps = 1000, strict = false)).result
+        bat_write(results_filename, samples)
+        samples2 = bat_read(results_filename).result
+        @test samples == samples2
+    end
 end
