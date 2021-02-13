9643c37d768fd785787e9c6e0cef5aefa6f95a12
Oliver Schulz <oschulz@mpp.mpg.de>
2021-02-14 00:41:32 +0100

    Update links in docs

--- docs/src/tutorial_lit.jl
+++ docs/src/tutorial_lit.jl
@@ -34,8 +34,8 @@
 # ["LinearAlgebra"](https://docs.julialang.org/en/v1/stdlib/LinearAlgebra/)
 # and ["Statistics"](https://docs.julialang.org/en/v1/stdlib/Statistics/),
 # as well as the packages
-# ["Distributions"](https://juliastats.github.io/Distributions.jl/stable/)
-# and ["StatsBase"](http://juliastats.github.io/StatsBase.jl/stable/):
+# ["Distributions"](https://juliastats.org/Distributions.jl/stable/)
+# and ["StatsBase"](https://juliastats.org/StatsBase.jl/stable/):
 
 using Random, LinearAlgebra, Statistics, Distributions, StatsBase
 
--- src/statistics/effective_sample_size.jl
+++ src/statistics/effective_sample_size.jl
@@ -69,7 +69,7 @@ end
     
 Integrated autocorrelation length estimation based on the automated windowing
 procedure descibed in
-[A. D. Sokal, "Monte Carlo Methods in Statistical Mechanics" (1996)](https://pdfs.semanticscholar.org/0bfe/9e3db30605fe2d4d26e1a288a5e2997e7225.pdf)
+[A. D. Sokal, "Monte Carlo Methods in Statistical Mechanics" (1996)](https://www.semanticscholar.org/paper/Monte-Carlo-Methods-in-Statistical-Mechanics%3A-and-Sokal/0bfe9e3db30605fe2d4d26e1a288a5e2997e7225)
 
 Same procedure is used by the emcee Python package (v3.0).
 
