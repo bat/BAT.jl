50104488093e4d62062df83200131d4c97319177
Scott Hayashi <scott.hayashi@tum.de>
2021-02-24 18:53:17 +0100

    Test SobolSampler

--- test/samplers/test_bat_sample.jl
+++ test/samplers/test_bat_sample.jl
@@ -32,4 +32,20 @@ using Random, Distributions, StatsBase
 
         isapprox(var(bat_sample(Normal(), BAT.IIDSampling(nsamples = 10^3)).result), [1], rtol = 10^-1)
     end
+
+    @testset "importance_sampling" begin
+        function test_moments(dist::AnyDensityLike, algo::BAT.AbstractSamplingAlgorithm)
+            samples = bat_sample(dist, algo).result # @inferred doesn't work
+
+            @test isapprox(@inferred(mean(samples)), @inferred(mean(dist)), rtol=0.01)
+            @test isapprox(@inferred(var(samples)), @inferred(var(dist)), rtol=0.01)
+        end
+
+        @testset "sobol_sampler" begin
+            dist = MvNormal([0.4, 0.6], [2.0 1.2; 1.2 3.0])
+            algo = SobolSampler(nsamples=10^5)
+
+            test_moments(dist, algo)
+        end
+    end
 end
