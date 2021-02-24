a580e54fb5d0adad89e472147bf5b88614682c09
Scott Hayashi <scott.hayashi@tum.de>
2021-02-24 20:15:51 +0100

    Test GridSampler

--- test/samplers/test_bat_sample.jl
+++ test/samplers/test_bat_sample.jl
@@ -34,11 +34,11 @@ using Random, Distributions, StatsBase
     end
 
     @testset "importance_sampling" begin
-        function test_moments(dist::AnyDensityLike, algo::BAT.AbstractSamplingAlgorithm)
+        function test_moments(dist::AnyDensityLike, algo::BAT.AbstractSamplingAlgorithm; rtol::Real=0.01)
             samples = bat_sample(dist, algo).result # @inferred doesn't work
 
-            @test isapprox(@inferred(mean(samples)), @inferred(mean(dist)), rtol=0.01)
-            @test isapprox(@inferred(var(samples)), @inferred(var(dist)), rtol=0.01)
+            @test isapprox(@inferred(mean(samples)), @inferred(mean(dist)), rtol=rtol)
+            @test isapprox(@inferred(var(samples)), @inferred(var(dist)), rtol=rtol)
         end
 
         @testset "sobol_sampler" begin
@@ -46,6 +46,13 @@ using Random, Distributions, StatsBase
             algo = SobolSampler(nsamples=10^5)
 
             test_moments(dist, algo)
+        end
+
+        @testset "grid_sampler" begin
+            dist = MvNormal([0.4, 0.6], [2.0 1.2; 1.2 3.0])
+            algo = GridSampler(ppa=500)
+
+            test_moments(dist, algo, rtol=0.1)
         end
     end
 end
