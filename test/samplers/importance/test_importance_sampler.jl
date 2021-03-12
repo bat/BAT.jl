e83aa5a4b5cc98e5b5aa7e137197c80764747a3b
Scott Hayashi <scott.hayashi@tum.de>
2021-03-13 00:26:12 +0100

    Removed @inferred from importance sampler tests

--- test/samplers/importance/test_importance_sampler.jl
+++ test/samplers/importance/test_importance_sampler.jl
@@ -8,7 +8,8 @@ using Random, Distributions, StatsBase
 
 @testset "importance_samplers" begin
     function test_moments(dist::AnyDensityLike, algo::BAT.AbstractSamplingAlgorithm; rtol::Real=0.01)
-        samples = @inferred(bat_sample(dist, algo)).result
+        # ToDo: Wrap in @inferred when type stable
+        samples = bat_sample(dist, algo).result
 
         @test isapprox(@inferred(mean(samples)), @inferred(mean(dist)), rtol=rtol)
         @test isapprox(@inferred(var(samples)), @inferred(var(dist)), rtol=rtol)
@@ -25,10 +26,6 @@ using Random, Distributions, StatsBase
         dist = MvNormal([0.4, 0.6], [2.0 1.2; 1.2 3.0])
         algo = GridSampler(ppa=500)
 
-        # ToDO: Use @inferred when type stable
-        samples = bat_sample(dist, algo).result
-        
-        @test isapprox(@inferred(mean(samples)), @inferred(mean(dist)), rtol=0.1)
-        @test isapprox(@inferred(var(samples)), @inferred(var(dist)), rtol=0.1)
+        test_moments(dist, algo, rtol=0.1)
     end
 end
