7921306d38f946402b9c7921a6bef3fea36c8b2d
Oliver Schulz <oschulz@mpp.mpg.de>
2021-04-14 10:21:10 +0200

    Add ForwardDiff utils

--- src/differentiation/differentiation.jl
+++ src/differentiation/differentiation.jl
@@ -1,4 +1,5 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
 
+include("forwarddiff_utils.jl")
 include("forwarddiff.jl")
---
+++ test/differentiation/test_differentiation.jl
@@ -0,0 +1,7 @@
+# This file is a part of BAT.jl, licensed under the MIT License (MIT).
+
+using Test
+
+Test.@testset "integration" begin
+    include("test_forwarddiff_utils.jl")
+end
