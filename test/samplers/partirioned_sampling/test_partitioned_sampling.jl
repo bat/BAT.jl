b2962d85f2da9077e18f31d9fb7ec5172bdc251b
jlrestrepol <jlrestrepol@gmail.com>
2021-04-20 16:01:36 +0200

    Runs all the basics and starts with tests

--- test/samplers/partirioned_sampling/test_partitioned_sampling.jl
+++ test/samplers/partirioned_sampling/test_partitioned_sampling.jl
@@ -3,13 +3,17 @@
 #Import Libraries
 using BAT
 using Test
+using Distributions, StatsBase, StructArrays
+using IntervalSets, ValueShapes, TypedTables, Random, LinearAlgebra
 
-############# Preliminars: define model, prior, likelihood and posterior ###############
+
+############# Preliminars: define model, prior, likelihood, posterior, algorithms and run partitioned sampling ###############
 
 ## Define the model, a multimode gaussian will be taken here
-σ = [0.1 0 0; 0 0.1 0; 0 0 0.1];
-μ = [1 1 -1; -1 1 0; -1 -1 -1; 1 -1 0];
-mixture_model = MixtureModel(MvNormal[MvNormal(μ[i,:], Matrix(Hermitian(σ)) ) for i in 1:4]);
+σ = [0.1 0 0; 0 0.1 0; 0 0 0.1]
+μ = [1 1 1; -1 1 0; -1 -1 -1; 1 -1 0]
+
+mixture_model = MixtureModel(MvNormal[MvNormal(μ[i,:], Matrix(Hermitian(σ)) ) for i in 1:4])
 
 #Define a flat prior
 prior = NamedTupleDist(a = [Uniform(-50,50), Uniform(-50,50), Uniform(-50,50)])
@@ -20,4 +24,21 @@ likelihood = let model = mixture_model
 end
 
 #Define the posterior
-posterior = PosteriorDensity(likelihood, prior);
+posterior = PosteriorDensity(likelihood, prior)
+
+#Sampling algorithms
+mcmc = MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, trafo = NoDensityTransform(),)#Subspace sampling
+mcmc_exp = MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, nchains=4, trafo = NoDensityTransform())#Exploratory samples
+ps = PartitionedSampling(sampler = mcmc, npartitions=4, exploration_sampler=mcmc_exp)#Space Partitioner
+
+#Sampling with space partition
+results = bat_sample(posterior , ps)
+############### End Preliminars ##################
+
+#Tests
+
+@testset "BAT_partitioned_sampling" begin
+    @testset "Exploratory Phase" begin
+        @test ps.exploration_sampler.nsteps == 10^4
+    end
+end
