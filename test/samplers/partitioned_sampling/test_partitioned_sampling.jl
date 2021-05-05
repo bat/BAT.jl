be02de3ef21e101fe31f259c760c4d523441e84a
jlrestrepol <jlrestrepol@gmail.com>
2021-05-05 16:18:37 +0200

    Changes exploration sampler to Sobol

--- test/samplers/partitioned_sampling/test_partitioned_sampling.jl
+++ test/samplers/partitioned_sampling/test_partitioned_sampling.jl
@@ -13,7 +13,7 @@ using Distributions, LinearAlgebra, ValueShapes
 μ = [1 1 1; -1 1 0; -1 -1 -1; 1 -1 0]
 mixture_model = MixtureModel(MvNormal[MvNormal(μ[i,:], Matrix(Hermitian(σ)) ) for i in 1:4])
 #Define a flat prior
-prior = NamedTupleDist(a = [Uniform(-2,2), Uniform(-2,2), Uniform(-2)])
+prior = NamedTupleDist(a = [Uniform(-2,2), Uniform(-2,2), Uniform(-2, 2)])
 #Define the Likelihood
 likelihood = let model = mixture_model
     params -> LogDVal(logpdf(model, params.a))
@@ -25,10 +25,10 @@ posterior = PosteriorDensity(likelihood, prior)
 mcmc = MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^3, trafo = NoDensityTransform(),);
 #vegas = BAT.VEGASIntegration(trafo = NoDensityTransform(), rtol = 0.001, atol = 1.0e-8)
 ahmi = ahmi = AHMIntegration(whitening = BAT.NoWhitening(), max_startingIDs = 10^3)
-#sobol = BAT.SobolSampler(nsamples = 500, trafo = NoDensityTransform())
-mcmc_exp = MCMCSampling(mcalg = MetropolisHastings(), nchains =4, nsteps = 400, trafo = NoDensityTransform(),);
+sobol = BAT.SobolSampler(nsamples = 500, trafo = NoDensityTransform())
+#mcmc_exp = MCMCSampling(mcalg = MetropolisHastings(), nchains =4, nsteps = 400, trafo = NoDensityTransform(),);
 
-ps = PartitionedSampling(sampler = mcmc, npartitions=4, exploration_sampler=mcmc_exp, integrator = ahmi);
+ps = PartitionedSampling(sampler = mcmc, npartitions=4, exploration_sampler=sobol, integrator = ahmi);
 ############### End Preliminars ##################
 
 #Tests
@@ -40,7 +40,7 @@ ps = PartitionedSampling(sampler = mcmc, npartitions=4, exploration_sampler=mcmc
     #Kolmogorov-Smirnov Test
     mcmc_samples = @inferred(bat_sample(posterior, mcmc))#sample from original pdf with MCMC, is that what you wanted?
     ks_test = bat_compare(mcmc_samples.result, results.result)
-    @test all(ks_test.result.ks_p_values .> 0.9)
+    @test all(ks_test.result.ks_p_values .> 0.7)
     @testset "Array of Posteriors" begin
         posteriors_array = BAT.convert_to_posterior(posterior, results.part_tree, extend_bounds = true)#Partition Posterior
 
