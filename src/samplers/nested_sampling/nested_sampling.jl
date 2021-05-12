ba7c456bcd1e833fe271724e74a4192c6d120fbb
Willy Weber <willy.weber@tu-dortmund.de>
2021-05-12 16:24:30 +0200

    Testfunktion

--- src/samplers/nested_sampling/nested_sampling.jl
+++ src/samplers/nested_sampling/nested_sampling.jl
@@ -1,3 +1,7 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
 # Included via @require: "ultranest.jl"
+
+function testtesttest()
+    println("Sie haben erfolgreich getestet")
+end
