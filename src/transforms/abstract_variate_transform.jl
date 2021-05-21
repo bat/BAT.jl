2dd20083c6184d64fd27c8b02aa3b0215a4cb4b4
Oliver Schulz <oschulz@mpp.mpg.de>
2021-05-21 23:31:02 +0200

    Add rrule for var_trafo_result
    Increases Zygote performance

--- src/transforms/abstract_variate_transform.jl
+++ src/transforms/abstract_variate_transform.jl
@@ -162,6 +162,17 @@ function var_trafo_result(trg_v::Any, src_v::Any, trafo_ladj::OptionalLADJ, prev
     (v = trg_v, ladj = trg_ladj)
 end
 
+function ChainRulesCore.rrule(::typeof(var_trafo_result), trg_v::Any, src_v::Any, trafo_ladj::OptionalLADJ, prev_ladj::OptionalLADJ)
+    result = var_trafo_result(trg_v, src_v, trafo_ladj, prev_ladj)
+    function var_trafo_result_pullback(thunked_ΔΩ)
+        ΔΩ = ChainRulesCore.unthunk(thunked_ΔΩ)
+        (ChainRulesCore.NO_FIELDS, ΔΩ.v, ZeroTangent(),
+            ismissing(trafo_ladj) ? missing : ΔΩ.ladj, ismissing(prev_ladj) ? missing : ΔΩ.ladj)
+    end
+    return result, var_trafo_result_pullback
+end
+
+
 
 # ToDo: Remove intermediate type `VariateTransform`?
 
