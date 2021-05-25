e47167c38048cd7ed7f1566162775ae38c2f2080
Willy Weber <willy.weber@tu-dortmund.de>
2021-05-25 20:30:13 +0200

    Now there is one version of an implementation of NestedSamplers in BAT.jl

--- src/samplers/nested_sampling/turing_nestedSamplers/turing_nestedSamplers.jl
+++ src/samplers/nested_sampling/turing_nestedSamplers/turing_nestedSamplers.jl
@@ -4,33 +4,15 @@ using .MCMCChains: Chains
 
 include("tns_bounds.jl")
 include("tns_proposals.jl")
-include("tns_utils.jl")
 
-@with_kw struct TuringNestedSamplersTR{TR<:AbstractDensityTransformTarget} <: AbstractSamplingAlgorithm
+@with_kw struct TuringNestedSamplers{TR<:AbstractDensityTransformTarget} <: AbstractSamplingAlgorithm
     trafo::TR = PriorToUniform()
 
     num_live_points::Int = 1000                         # the number of live-points
     bound::TNS_Bound = TNS_MultiEllipsoidBound()        # volume around the live-points
     proposal::TNS_Proposal = TNS_AutoProposal()         # algorithm to choose new live-points
-    enlarge::Float64 = 1.25                             # Scale-factor for the volume
-    # update_interval::Float64 =                        # Not sure about how this works yet
-    min_ncall::Int64 = 2*num_live_points                # number of iterations before the first bound will be fit
-    min_eff::Float64 = 0.10                             # efficiency before fitting the first bound
-
-    # The following four are the possible convergence criteria to end the algorithm
-    dlogz::Float64 = 0.1
-    max_iters = Inf
-    max_ncalls = Inf
-    maxlogl = Inf
-end
-export TuringNestedSamplersTR
-
-@with_kw struct TuringNestedSamplers <: AbstractSamplingAlgorithm
-    num_live_points::Int = 1000                         # the number of live-points
-    bound::TNS_Bound = TNS_MultiEllipsoidBound()        # volume around the live-points
-    proposal::TNS_Proposal = TNS_AutoProposal()         # algorithm to choose new live-points
-    enlarge::Float64 = 1.25                             # Scale-factor for the volume
-    # update_interval::Float64 =                        # Not sure about how this works yet
+    enlarge::Float64 = 1.25                             # scale-factor for the volume
+    # update_interval::Float64 =                        # not sure about how this works yet
     min_ncall::Int64 = 2*num_live_points                # number of iterations before the first bound will be fit
     min_eff::Float64 = 0.10                             # efficiency before fitting the first bound
 
@@ -48,32 +30,12 @@ export TuringNestedSamplers
 ############################################################################################################
 function bat_sample_impl(rng::AbstractRNG, target::AnyDensityLike, algorithm::TuringNestedSamplers)
     
-    posterior = convert(AbstractDensity, target)
-    model, sampler = batPosterior_to_nestedModel(posterior; 
-        algorithm.num_live_points, algorithm.bound, algorithm.proposal,
-        algorithm.enlarge, algorithm.min_ncall, algorithm.min_eff
-    )
-    
-    chain, state = sample(model, sampler; 
-        dlogz = algorithm.dlogz, maxiter = algorithm.max_iters,
-        maxcall = algorithm.max_ncalls, maxlogl = algorithm.maxlogl, chain_type=Chains
-    )
-
-    res = chain_to_batsamples(chain, posterior);
-    return ( ########### Here is more to do i think
-        result = res,
-    )
-end
-
-# This version does the prior transformation in BAT instead of NestedSamplers. Thus, it fits better in the package.
-function bat_sample_impl(rng::AbstractRNG, target::AnyDensityLike, algorithm::TuringNestedSamplersTR)
-    
     density_notrafo = convert(AbstractDensity, target)
     shaped_density, trafo = bat_transform(algorithm.trafo, density_notrafo)                 # BAT prior transformation
     vs = varshape(shaped_density)
     density = unshaped(shaped_density)
 
-    # This function is used for likelihood calculations by NestedSamplers. It has to be a fuction of only x. It includes the Prior.
+    # This function is used for likelihood calculations by NestedSamplers. It has to be a fuction of only x. It includes the prior.
     function nestedSamplers_Likelihood(x)
         return BAT.eval_logval_unchecked(density,x)
     end
