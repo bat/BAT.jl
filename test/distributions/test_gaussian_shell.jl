9f535b55bc5dd5b48033993b279606bcfd552dc7
jlrestrepol <jlrestrepol@gmail.com>
2021-05-28 11:58:11 +0200

    Adds simple tests to gaussian shell

---
+++ test/distributions/test_gaussian_shell.jl
@@ -0,0 +1,21 @@
+using BAT
+using Test
+using Statistics
+
+@testset "Gaussian Shell Distribution" begin
+    #Tests different ways of instantiate GaussianShell Distribution
+    @test @inferred(BAT.GaussianShell(r = 3., w = 0.1, n = 3)) isa BAT.GaussianShell
+    @test @inferred(BAT.GaussianShell()) isa BAT.GaussianShell
+
+    gs = BAT.GaussianShell(r = 3., w = 0.1, n = 3)
+
+    @test @inferred(Base.length(gs)) == 3
+    @test @inferred(Base.eltype(gs)) == Float64
+
+    @test @inferred(StatsBase.params(gs)) == (3.0, 0.1, [0.0, 0.0, 0.0])
+
+    @test isapprox(@inferred(Statistics.cov(gs)), [3.02718 0.00166016 0.0113455; 0.00166016 3.01293 0.00453035; 0.0113455   0.00453035  3.01046])
+
+    @test isapprox(@inferred(Distributions._logpdf(gs, [0., 0., 0.])), -453.34571, atol = 1e-5)
+
+end
