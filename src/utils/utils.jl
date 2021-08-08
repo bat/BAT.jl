10301ed81610b9584bb7998fe67b5782c5129613
Oliver Schulz <oschulz@mpp.mpg.de>
2021-08-08 23:50:00 +0200

    Add executors and exec_map!

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -6,3 +6,6 @@ include("array_utils.jl")
 include("convolution_utils.jl")
 include("coord_utils.jl")
 include("valueshapes_utils.jl")
+include("executors.jl")
+
+# Included via @require: "executors_folds.jl")
