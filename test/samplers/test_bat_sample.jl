bb59619cd17fca5c3b2f037bae799c262884e9eb
Oliver Schulz <oschulz@mpp.mpg.de>
2021-08-12 14:04:19 +0200

    Improve RandResampling tests

--- test/samplers/test_bat_sample.jl
+++ test/samplers/test_bat_sample.jl
@@ -29,22 +29,21 @@ using Random, Distributions, StatsBase
 
         isapprox(var(bat_sample(Normal(), BAT.IIDSampling(nsamples = 10^3)).result), [1], rtol = 10^-1)
     end
+
     @testset "RandResampling" begin
         dist = Normal()
         result = @inferred(bat_sample(dist, IIDSampling(nsamples = 2))).result #Draw to samples from Normal dist
-
-        
 
         @test @inferred(bat_sample(Random.GLOBAL_RNG, result)).result isa DensitySampleVector#Check data types 
         @test @inferred(bat_sample(result, RandResampling(nsamples = 100))).result isa DensitySampleVector
         @test @inferred(bat_sample(Random.GLOBAL_RNG, result, BAT.RandResampling())).result isa DensitySampleVector
 
-        
-        samples_rdm = @inferred(bat_sample(result, RandResampling(nsamples = 10^5))).result.v#Sample 100 times from the 2-sample space
+        samples_rdm = @inferred(bat_sample(result, RandResampling(nsamples = 10^5))).result #Sample 100 times from the 2-sample space
         @test length(@inferred(bat_sample(result, RandResampling(nsamples = 100))).result) == 100#Check shape is ok
-        @test sort(unique(samples_rdm)) == sort(result.v)#check it only samples from the 2-sample space
-        @test isapprox(mean(samples_rdm), mean(result.v), rtol = 10^-1)#means should be the same in both datasets
+        @test sort(unique(samples_rdm.v)) == sort(result.v)#check it only samples from the 2-sample space
+        @test isapprox(mean(samples_rdm), mean(result), rtol = 10^-1)#means should be the same in both datasets
     end
+
     @testset "OrderedResampling" begin #Creates new testset for OrderedResampling
         dist = MvNormal([0.4, 0.6], [2.0 1.2; 1.2 3.0])
         result = @inferred(bat_sample(dist, IIDSampling(nsamples = 10^5))).result
@@ -57,6 +56,5 @@ using Random, Distributions, StatsBase
 
         resamples = @inferred(bat_sample(result, OrderedResampling(nsamples = length(result)))).result
         @test result == resamples
-        
     end
 end
