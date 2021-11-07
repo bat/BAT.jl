4ebf99057c3414bdc1b7058a4fbdbd78ac18920e
Oliver Schulz <oschulz@mpp.mpg.de>
2021-11-07 19:29:59 +0100

    Disable extended HMC tests for now
    Current implementation of extended HMC tests results in excessive
    test run time.

--- test/samplers/mcmc/test_hmc.jl
+++ test/samplers/mcmc/test_hmc.jl
@@ -3,12 +3,9 @@ using BAT
 using Test
 
 using LinearAlgebra
-using HypothesisTests
-using IntervalSets
 using StatsBase, Distributions, StatsBase, ValueShapes, ArraysOfArrays, DensityInterface
 
 @testset "HamiltonianMC" begin
-
     rng = bat_rng()
     target = NamedTupleDist(a = Normal(1, 1.5), b = MvNormal([-1.0, 2.0], [2.0 1.5; 1.5 3.0]))
 
@@ -134,140 +131,5 @@ using StatsBase, Distributions, StatsBase, ValueShapes, ArraysOfArrays, DensityI
         # Test with nested posteriors:
         posterior = PosteriorDensity(likelihood, inner_posterior)
         @test BAT.sample_and_verify(posterior, MCMCSampling(mcalg = HamiltonianMC(), trafo = PriorToGaussian()), prior.dist).verified
-    end
-
-    @testset "sampling" begin
-        function test_sampler(;
-            num_dims::Integer = 2,
-            sampling_algo=MCMCSampling(mcalg=HamiltonianMC()),
-            test_name::String
-            )
-            @testset "$test_name" begin
-                μ = rand(num_dims)
-                σ = rand(num_dims) |> x -> abs.(x)
- 
-                normal_dists = Vector{Normal{eltype(σ)}}(undef, num_dims)
-                for i in eachindex(normal_dists)
-                    normal_dists[i] = Normal(μ[i], σ[i])
-                end
-        
-                mvnormal = product_distribution(normal_dists)
-        
-                samples = BAT.sample_and_verify(mvnormal, sampling_algo)
-
-                @testset "ahmi integration" begin
-                    integral_res = bat_integrate(samples.result).result
-                    integral_val = integral_res.val
-                    integral_err = integral_res.err
-        
-                    @test isapprox(1, integral_val, rtol=7*integral_err)
-                end
-
-                @testset "sampler test" begin
-                    @test samples.verified
-                end
-    
-                @testset "chains" begin
-                    chains = samples.generator.chains
-                    num_chains = @inferred(length(chains))
-
-                    @test num_chains == sampling_algo.nchains
-
-                    for chain in chains
-                        chain_info = chain.info
-
-                        @test @inferred(BAT.mcmc_info(chain)) == chain_info
-                        @test chain_info.converged == true
-                        @test chain_info.tuned == true
-                    end
-                end
-            end
-        end
-        @testset "ahmc kernels" begin
-            tuning = BAT.StanHMCTuning(target_acceptance=0.999)
-            mcalg = HamiltonianMC(tuning=tuning)
-
-            burnin = MCMCMultiCycleBurnin(max_ncycles=100)
-            chain_init = MCMCChainPoolInit(init_tries_per_chain=8..500, nsteps_init=1000)
-
-            sampling_algo = MCMCSampling(nchains=8, trafo=BAT.NoDensityTransform(), init=chain_init, burnin=burnin, mcalg=mcalg)
-
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="NUTS")
-        end
-        
-        @testset "ahmc integrators" begin
-            tuning = BAT.StanHMCTuning(target_acceptance=0.999)
-            burnin = MCMCMultiCycleBurnin(max_ncycles=100)
-            chain_init = MCMCChainPoolInit(init_tries_per_chain=8..600, nsteps_init=2000)
-
-            mcalg_lf = HamiltonianMC(integrator=BAT.LeapfrogIntegrator(1.0), tuning=tuning)
-            sampling_algo = MCMCSampling(nchains=8, init=chain_init, burnin=burnin, mcalg=mcalg_lf)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="leapfrog")
-
-            mcalg_jittered_lf = HamiltonianMC(integrator=BAT.JitteredLeapfrogIntegrator(), tuning=tuning)
-            sampling_algo = MCMCSampling(nchains=8, init=chain_init, burnin=burnin, mcalg=mcalg_jittered_lf)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="jittered leapfrog")
-
-            mcalg_tempered_lf = HamiltonianMC(integrator=BAT.TemperedLeapfrogIntegrator(), tuning=tuning)
-            sampling_algo = MCMCSampling(nchains=8, init=chain_init, burnin=burnin, mcalg=mcalg_tempered_lf)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="tempered leapfrog")
-        end
-
-        @testset "ahmc metrics" begin
-            tuning = BAT.StanHMCTuning(target_acceptance=0.999)
-
-            burnin = MCMCMultiCycleBurnin(max_ncycles=100)
-            chain_init = MCMCChainPoolInit(init_tries_per_chain=8..600, nsteps_init=2000)
-
-            mcalg_unit_euc = HamiltonianMC(metric=BAT.UnitEuclideanMetric(), tuning=tuning)
-            sampling_algo = MCMCSampling(nchains=8, init=chain_init, burnin=burnin, mcalg=mcalg_unit_euc)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="unit euclidean")
-
-            mcalg_dense_euc = HamiltonianMC(metric=BAT.DenseEuclideanMetric(), tuning=tuning)
-            sampling_algo = MCMCSampling(init=chain_init, burnin=burnin, mcalg=mcalg_dense_euc)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="dense euclidean")
-        end
-
-        @testset "ahmc adaptors" begin
-            burnin = MCMCMultiCycleBurnin(max_ncycles=100)
-            chain_init_more = MCMCChainPoolInit(init_tries_per_chain=8..600, nsteps_init=2000)
-
-            mcalg_massmat = HamiltonianMC(tuning=BAT.MassMatrixAdaptor(0.1), metric=BAT.UnitEuclideanMetric())
-            sampling_algo = MCMCSampling(mcalg=mcalg_massmat)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="mass matrix")
-
-            mcalg_stepsize = HamiltonianMC(tuning=BAT.StepSizeAdaptor())
-            sampling_algo = MCMCSampling(nchains=4, init=chain_init_more, trafo=BAT.NoDensityTransform(), mcalg=mcalg_stepsize)
-            test_sampler(num_dims=2, sampling_algo=sampling_algo, test_name="step size")
-
-            # Add BAT.NaiveHMCTuning when implemented
-        end
-    end
-
-    @testset "ahmc sampleid" begin
-        mvnorm = @inferred(product_distribution([Normal(), Normal()]))
-        sampling_algo = @inferred(MCMCSampling(mcalg=HamiltonianMC(), nchains=2, trafo=NoDensityTransform(), nsteps=10^3))
-    
-        # Use @inferred when type stable
-        samples_1 = bat_sample(mvnorm, sampling_algo).result
-        samples_2 = bat_sample(mvnorm, sampling_algo).result
-    
-        id_vector_1 = samples_1.info
-        id_vector_2 = samples_2.info
-        id_vector_12 = @inferred(merge(id_vector_1, id_vector_2))
-    
-        num_sample_ids_1 = @inferred(length(id_vector_1))
-        num_sample_ids_2 = @inferred(length(id_vector_2))
-    
-        @test @inferred(length(id_vector_12)) == num_sample_ids_1 + num_sample_ids_2
-    
-        @test id_vector_12[1:num_sample_ids_1] == id_vector_1
-        @test id_vector_12[num_sample_ids_1+1:end] == id_vector_2
-    
-        empty_sample_idvec = @inferred(BAT.MCMCSampleIDVector())
-        @test @inferred(isempty(empty_sample_idvec))
-    
-        merge!(id_vector_1, id_vector_2)
-        @test id_vector_1 == id_vector_12
     end
 end
