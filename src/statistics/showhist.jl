f9e8ce976a8e88ac2cdf8bd3df29a6318b4c1ad8
Oliver Schulz <oschulz@mpp.mpg.de>
2022-07-03 04:05:57 +0200

    Add marginal_histograms

--- src/statistics/showhist.jl
+++ src/statistics/showhist.jl
@@ -13,8 +13,8 @@ function showhist_unicode(io::IO, h::Histogram{<:Real,1}; bar_symbols::AbstractV
     norm_factor = length(symidxs) / maximum(W)
     get_sym_idx(x) = isnan(x) ? 1 : clamp(first(symidxs) + floor(Int, norm_factor * x), first(symidxs), last(symidxs))
 
-    mean_idx = StatsBase.binindex(h, mean(X, FrequencyWeights(W)))
-    median_idx = StatsBase.binindex(h, median(X, FrequencyWeights(W)))
+    mean_idx = StatsBase.binindex(h, mean(X, Weights(W)))
+    median_idx = StatsBase.binindex(h, median(X, Weights(W)))
     
     print(io, replace(lpad(@sprintf("%8.3g", minimum(edge)), 8, '\u2800'), " " => "\u2800"))
     print(io, h.closed == :left ? "[" : "]")
@@ -33,4 +33,19 @@ function showhist_unicode(io::IO, h::Histogram{<:Real,1}; bar_symbols::AbstractV
     end
     print(io, h.closed == :right ? "]" : "[")
     print(io, replace(lpad(@sprintf("%-8.3g", maximum(edge)), 8, '\u2800'), " " => "\u2800"))
+end
+
+
+function hist_unicode(h::Histogram{<:Real,1}; kwargs...)
+    io = IOBuffer()
+    showhist_unicode(io, h; kwargs...)
+    String(take!(io))
+end
+
+
+function marginal_histograms(smpl::DensitySampleVector{<:AbstractVector{<:Real}}; nbins = 40)
+    trimmed_smpl = drop_low_weight_samples(smpl)
+    V = flatview(trimmed_smpl.v)
+    W = Weights(trimmed_smpl.weight)
+    [fit(Histogram, V[i,:], W, range(minimum(V[i,:]), maximum(V[i,:]), length = 41)) for i in axes(V,1)]
 end
