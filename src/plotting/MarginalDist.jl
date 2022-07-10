faa34f4a4b29072fa8a8a20960767f1076e9bcce
Micki-D <michael.dudkowiak@tum.de>
2022-07-10 16:23:20 +0200

    fix edge generation for bins

--- src/plotting/MarginalDist.jl
+++ src/plotting/MarginalDist.jl
@@ -45,7 +45,7 @@ function MarginalDist(
     elseif bins isa Tuple
         Tuple(_get_edges(cols[i], bins[i], closed) for i in 1:length(bins))
     else 
-        _get_edges(cols, bins, closed)
+        Tuple(_get_edges(cols, bins, closed))
     end
 
     hist = fit(Histogram, cols, FrequencyWeights(samples.weight), edges, closed = closed)
