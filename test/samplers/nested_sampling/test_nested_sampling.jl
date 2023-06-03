f2296b4c8c4926fab57230c281b5082c41d12e78
Oliver Schulz <oschulz@mpp.mpg.de>
2023-06-04 00:38:05 +0200

    Enable UltraNest tests on newer Julia versions only

--- test/samplers/nested_sampling/test_nested_sampling.jl
+++ test/samplers/nested_sampling/test_nested_sampling.jl
@@ -6,8 +6,9 @@ import Pkg
 
 Test.@testset "nested_sampling" begin
     # Python ultranest package doesn't seem to be available via Conda on
-    # 32-bit systems:
-    if Int == Int64 && ("UltraNest" in keys(Pkg.project().dependencies))
+    # 32-bit systems, GLIBCXX version conflicts cause trouble for CI
+    # on older Julia versions:
+    if Int == Int64 && VERSION > v"1.8" && ("UltraNest" in keys(Pkg.project().dependencies))
         include("test_ultranest.jl")
     end
     if ("NestedSamplers" in keys(Pkg.project().dependencies))
