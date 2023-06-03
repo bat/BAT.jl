7535ec4ab1c4328f5ab0fbc1c84a84075da6c79a
Oliver Schulz <oschulz@mpp.mpg.de>
2023-06-03 22:42:16 +0200

    Only run nested sampling tests if packages available

--- test/samplers/nested_sampling/test_nested_sampling.jl
+++ test/samplers/nested_sampling/test_nested_sampling.jl
@@ -2,13 +2,15 @@
 
 using Test
 
+import Pkg
+
 Test.@testset "nested_sampling" begin
     # Python ultranest package doesn't seem to be available via Conda on
     # 32-bit systems:
-    if Int == Int64
-        if isdefined(Main, :UltraNest)
-            include("test_ultranest.jl")
-        end
+    if Int == Int64 && ("UltraNest" in keys(Pkg.project().dependencies))
+        include("test_ultranest.jl")
+    end
+    if ("NestedSamplers" in keys(Pkg.project().dependencies))
         include("test_ellipsoidal_nested_sampling.jl")
     end
 end
