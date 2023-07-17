21e0cb14aa8e16a0759331e5b90a9d9688808d48
waldie11 <86674066+waldie11@users.noreply.github.com>
2023-07-17 21:21:41 +0200

    unittests for bat_initval(…,::InitFromSamples)

--- test/initvals/test_initvals.jl
+++ test/initvals/test_initvals.jl
@@ -17,6 +17,8 @@ using Distributions, PDMats, StatsBase, ValueShapes, ArraysOfArrays
         @test pdf(du, bat_initval(du, context).result) > 0
         @test @inferred(bat_initval(du, 10, context)).result isa VectorOfSimilarVectors{<:AbstractFloat}
         @test all(x -> x > 0, pdf.(Ref(du), bat_initval(du, 10, context).result))
+        v_u = BAT.bat_sample(du, BAT.IIDSampling(nsamples=1)).result
+        @test BAT.bat_initval(du, BAT.InitFromSamples(v_u), context).result == first(v_u.v)
 
         ds = NamedTupleDist(a = Normal(), b = Exponential(), c = Uniform(-1, 2))
         @test @inferred(bat_default(bat_initval, Val(:algorithm), du)) == InitFromTarget()
@@ -25,5 +27,7 @@ using Distributions, PDMats, StatsBase, ValueShapes, ArraysOfArrays
         @test pdf(ds, bat_initval(ds, context).result) > 0
         @test @inferred(bat_initval(ds, 10, context)).result isa ShapedAsNTArray
         @test all(x -> x > 0, pdf.(Ref(ds), bat_initval(ds, 10, context).result))
+        v_s = BAT.bat_sample(du, BAT.IIDSampling(nsamples=1)).result
+        @test BAT.bat_initval(du, BAT.InitFromSamples(v_s), context).result == first(v_s.v)
     end
 end
