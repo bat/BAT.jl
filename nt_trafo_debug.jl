bc86a018cdad1a8e90fa48b1f20b2d908278d7d6
Oliver Schulz <oschulz@mpp.mpg.de>
2023-11-18 00:37:23 +0100

    DEBUG

--- nt_trafo_debug.jl
+++ nt_trafo_debug.jl
@@ -18,33 +18,11 @@ prior = BAT.distprod(parameter_nt,)
 
 
 using BAT: apply_dist_trafo, _flat_ntd_eff_accessors, _stdmv_to_flat_ntdistelem, _flat_ntd_orig_accessors,
-    eff_totalndof, StdMvDist
+    eff_totalndof, StdMvDist, _stddist, _elem_from_std
 
 trg_d = prior;
 src_d = BAT.StandardMvUniform(totalndof(varshape(trg_d)))
 src_v = rand(src_d)
-
-nms = propertynames(trg_d)
-
-
-trg_dists = values(trg_d)
-N = length(trg_dists)
-n_vars = [Symbol("n$i") for i in 1:N]
-d_vars = [Symbol("d$i") for i in 1:N]
-r_vars = [Symbol("r$i") for i in 1:N]
-i = 1
-
-_resize_stdmv(::T, n::Int) where {T<:StdMvDist} = T(n)
-
-quote
-    src_v_pos = firstindex(src_v)
-end
-quote
-    $(d_vars[i]) = trg_dists[$i]
-    $(n_vars[i]) = eff_totalndof($(d_vars[i]))
-    $(r_vars[i]) = apply_dist_trafo($(d_vars[i]), _resize_stdmv(src_d, $(n_vars[i])), view(src_v, src_v_pos:src_v_pos+$(n_vars[i])-1))
-    src_v_pos += $(n_vars[i])
-end
 
 
 # create some random samples and put into DensitySampleVector
