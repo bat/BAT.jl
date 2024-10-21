df815c97cfa54baa4925b4110a9ba650e061adf5
Oliver Schulz <oschulz@mpp.mpg.de>
2024-10-21 12:41:51 +0200

    Base example posteriors on generated forward model and data
    Density of examples posteriors remains unchanged.

---
+++ src/measures/example_posterior.jl
@@ -0,0 +1,42 @@
+# This file is a part of BAT.jl, licensed under the MIT License (MIT).
+
+
+example_stable_rng() = StableRNGs.StableRNG(0x4cf83495c736cac2)
+
+
+function example_prior()
+    return NamedTupleDist(
+        a = Exponential(),
+        b = [4.2, 3.3],
+        c = Normal(1, 3),
+        d = [Weibull(), Weibull()],
+        e = Beta(),
+        f = MvNormal([0.3, -2.9], [1.7 0.5; 0.5 2.3])
+    )
+end
+
+function example_prior_with_dirichlet()
+    merge(example_prior(), (g = Dirichlet([1.2, 2.4, 3.6]),))
+end
+
+
+function example_likelihood(prior::Distribution, rng::AbstractRNG)
+    n = totalndof(varshape(prior))
+    A = randn(rng, n, n)
+    Σ = A * A'
+    f_transform = inverse(varshape(prior))
+    f_model = Base.Fix2(MvNormal, Σ) ∘ f_transform
+    tmp_rng = deepcopy(rng)
+    data = zero(f_transform(rand(tmp_rng, prior)))
+    return Likelihood(f_model, data)
+end
+
+
+function example_posterior(rng::AbstractRNG = example_stable_rng(), prior::Distribution = example_prior())
+    likelihood = example_likelihood(prior, rng)
+    lbqintegral(likelihood, prior)
+end
+
+function example_posterior_with_dirichlet(rng::AbstractRNG = example_stable_rng())
+    return example_posterior(rng, example_prior_with_dirichlet())
+end
--- src/measures/posterior_measure.jl
+++ src/measures/posterior_measure.jl
@@ -152,30 +152,3 @@ end
 
 
 (shape::AbstractValueShape)(density::PosteriorMeasure) = PosteriorMeasure(shape(density.likelihood), shape(density.prior))
-
-
-function example_posterior()
-    rng = StableRNGs.StableRNG(0x4cf83495c736cac2)
-    prior = NamedTupleDist(
-        b = [4.2, 3.3],
-        a = Exponential(),
-        c = Normal(1, 3),
-        d = [Weibull(), Weibull()],
-        e = Beta(),
-        f = MvNormal([0.3, -2.9], [1.7 0.5; 0.5 2.3])
-    )
-    n = totalndof(varshape(prior))
-    A = randn(rng, n, n)
-    likelihood = logfuncdensity(logdensityof(varshape(prior)(MvNormal(A * A'))))
-    lbqintegral(likelihood, prior)
-end
-
-
-function example_posterior_with_dirichlet()
-    rng = StableRNGs.StableRNG(0x4cf83495c736cac2)
-    prior = merge(BAT.example_posterior().prior.dist, (g = Dirichlet([1.2, 2.4, 3.6]),))
-    n = totalndof(varshape(prior))
-    A = randn(rng, n, n)
-    likelihood = logfuncdensity(logdensityof(varshape(prior)(MvNormal(A * A'))))
-    PosteriorMeasure(likelihood, prior)
-end
