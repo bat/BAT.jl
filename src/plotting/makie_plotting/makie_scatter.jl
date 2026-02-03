9833950fd9f0a0cec562bf6b2d71901238cc2062
Michael Dudkowiak <michael.dudkowiak@tum.de>
2026-02-03 10:31:07 +0100

    Refactor makie scatter for BAT infrastructure

--- src/plotting/makie_plotting/makie_scatter.jl
+++ src/plotting/makie_plotting/makie_scatter.jl
@@ -1,6 +1,7 @@
 @recipe(Scatter2D, x, y) do scene
     Attributes(
         weights = nothing,
+        filter = false,
         color = Makie.wong_colors()[1],
         alpha = 1.0,
         markersize = 2.0
@@ -8,22 +9,44 @@
 end
 
 function Makie.plot!(p::Scatter2D)
-    real_markersize = lift(p.weights, p.markersize) do w, base_size
-        if isnothing(w) || w isa Number
-            return base_size
-        else
-            w_vals = w isa AbstractWeights ? w.values : w
+    data = lift(p.samples, p.vsel, p.filter) do smpls, vsel, f
+        marg = bat_marginalize(s, vsel)
+        marg_res = marg.result
 
-            mean_w = mean(w_vals)
-            if mean_w <= 0
-                return base_size
-            end
-
-            return sqrt.(w_vals ./ mean_w) .* base_size
+        if f
+            marg_res = drop_low_weight_samples(marg_res)
         end
+
+        flat_vals = flatview(unshaped.(marg_res).v)
+        x = flat_vals[1, :]
+        y = flat_vals[2, :]
+        w = marg_res.weight
+
+        return (x, y, w)
     end
 
-    scatter!(p, p[1], p[2];
+    x_vals = lift(d -> d[1], data)
+    y_vals = lift(d -> d[2], data)
+    w_vals = lift(d -> d[3], data)
+
+    real_markersize = lift(w_vals, p.markersize) do w, base_size
+        if isempty(w)
+            return base_size
+        end
+
+        if all(x -> x ≈ w[1], w)
+            return base_size
+        end
+
+        mean_w = mean(w)
+        if mean_w <= 0
+            return base_size
+        end
+
+        return sqrt.(w ./ mean_w) .* base_size
+    end
+
+    scatter!(p, x_vals, y_vals;
         color = p.color,
         alpha = p.alpha,
         markersize = real_markersize
