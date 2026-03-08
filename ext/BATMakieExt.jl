0063605ae8dea3957b0a919c4fed62fe8ad295f4
Michael Dudkowiak <michael.dudkowiak@tum.de>
2026-03-08 12:27:41 +0100

    Tie the Makie extension module into BAT

--- ext/BATMakieExt.jl
+++ ext/BATMakieExt.jl
@@ -12,6 +12,7 @@ using ValueShapes
 using BAT: MarginalDist, BAT.get_bin_centers, get_smallest_intervals, drop_low_weight_samples
 using BAT: bat_marginalize
 using BAT: asindex, getstring
+import BAT: bat_makie_plot, bat_makie_plot!
 
 include("./makie_impl/makie_plotting.jl")
 
